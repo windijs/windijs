f31972bc6e48e28cc15949ce40f3fdd804f0b3c8
voorjaar <felitas@zoho.com>
2022-07-07 19:11:14 +0800

    add some utils

--- src/utils.ts
+++ src/utils.ts
@@ -12,7 +12,7 @@ export function isNumber (value: string) {
 
 export const hasKey = <T extends object>(obj: T, k: keyof any): k is keyof T => k in obj;
 
-export function useProxy<T extends object> (f: (prop: string) => StyleObject | undefined) {
+export function useProxy<T extends object, S = StyleObject> (f: (prop: string) => S | undefined) {
   const handler: ProxyHandler<T> = {
     get (target, prop) {
       return f(prop as string);
@@ -20,4 +20,16 @@ export function useProxy<T extends object> (f: (prop: string) => StyleObject | u
   };
 
   return new Proxy({}, handler);
+}
+
+export function commaJoin (...items: (string|number|undefined)[]) {
+  return items.filter(i => i).join(", ");
+}
+
+export function parenWrap (key: string, value: string) {
+  return key + "(" + value + ")";
+}
+
+export function camelToDash (str: string) {
+  return str.replace(/([A-Z])/g, val => `-${val.toLowerCase()}`);
 }
