73a57ea4f94d3d71e57c4f5c5f56de0b040557a4
voorjaar <felitas@zoho.com>
2022-07-07 10:46:35 +0800

    config for bg

---
+++ src/config/bg.ts
@@ -0,0 +1,48 @@
+export const backgroundAttachmentConfig = {
+  fixed: "fixed",
+  local: "local",
+  scroll: "scroll",
+};
+
+export const backgroundClipConfig = {
+  border: "border-box",
+  padding: "padding-box",
+  content: "content-box",
+};
+
+export const backgroundPositionConfig = {
+  bottom: "bottom",
+  center: "center",
+  left: {
+    "": "left",
+    bottom: "left bottom",
+    top: "left top",
+  },
+  right: {
+    "": "right",
+    bottom: "right bottom",
+    top: "right top",
+  },
+  top: "top",
+};
+
+export const backgroundRepeatConfig = {
+  "": "repeat",
+  not: "no-repeat",
+  x: "repeat-x",
+  y: "repeat-y",
+  round: "round",
+  space: "space",
+};
+
+export const backgroundSizeConfig = {
+  auto: "auto",
+  cover: "cover",
+  contain: "contain",
+};
+
+export const backgroundOriginConfig = {
+  border: "border-box",
+  padding: "padding-box",
+  content: "content-box",
+};
