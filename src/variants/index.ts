706eec523488a589463d1bbc46308380a9037058
voorjaar <felitas@zoho.com>
2022-07-08 17:04:02 +0800

    add variant entry

---
+++ src/variants/index.ts
@@ -0,0 +1,6 @@
+export * from "./base";
+export * from "./pseudo-class";
+export * from "./pseudo-element";
+export * from "./parent-selector";
+export * from "./child-selector";
+export * from "./media-query";
