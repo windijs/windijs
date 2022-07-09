ae89224b2b5e68dc5b20886696b1c557b7b995ad
voorjaar <felitas@zoho.com>
2022-07-09 21:59:36 +0800

    test new variant api

--- tests/variants/base.test.ts
+++ tests/variants/base.test.ts
@@ -1,4 +1,4 @@
-import { createUtility, backgroundColor, colors, backgroundClip, backgroundClipConfig, useVariant, useMedia } from "../../src";
+import { createUtility, backgroundColor, colors, backgroundClip, backgroundClipConfig, useVariant, useMedia, variant, media } from "../../src";
 
 const bg = createUtility()
   .use(backgroundColor(colors))
@@ -15,4 +15,14 @@ test("useVariant", () => {
 test("useMedia", () => {
   expect(useMedia("print", utilities)).toMatchSnapshot();
   expect(useMedia("(min-width: 1000px) and (max-width: 2000px)", utilities)).toMatchSnapshot();
+});
+
+test("variant", () => {
+  expect(variant("span", bg.green[500], bg.clip.content)).toMatchSnapshot();
+  expect(variant("&:hover", bg.green[500], bg.clip.content)).toMatchSnapshot();
+});
+
+test("media", () => {
+  expect(media("print", bg.green[500], bg.clip.content)).toMatchSnapshot();
+  expect(media("(min-width: 1000px) and (max-width: 2000px)", bg.green[500], bg.clip.content)).toMatchSnapshot();
 });
