c3c2a558db2a140e0812fe02c6a8733cb856b30d
voorjaar <felitas@zoho.com>
2022-07-09 21:38:35 +0800

    test child selectors

---
+++ tests/variants/child-selector.test.ts
@@ -0,0 +1,28 @@
+import { createUtility, backgroundColor, colors, backgroundClip, backgroundClipConfig, svg, all, children, siblings, sibling } from "../../src";
+
+const bg = createUtility()
+  .use(backgroundColor(colors))
+  .use(backgroundClip(backgroundClipConfig))
+  .init();
+
+const utilities = [bg.blue[500], bg.clip.content];
+
+test("svg", () => {
+  expect(svg(...utilities)).toMatchSnapshot();
+});
+
+test("all", () => {
+  expect(all(...utilities)).toMatchSnapshot();
+});
+
+test("children", () => {
+  expect(children(...utilities)).toMatchSnapshot();
+});
+
+test("siblings", () => {
+  expect(siblings(...utilities)).toMatchSnapshot();
+});
+
+test("sibling", () => {
+  expect(sibling(...utilities)).toMatchSnapshot();
+});
