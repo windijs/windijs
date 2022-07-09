717338d30d1dc98c90a2b0d8b8bec411595d2aec
voorjaar <felitas@zoho.com>
2022-07-09 21:38:14 +0800

    test parent selectors

---
+++ tests/variants/parent-selector.test.ts
@@ -0,0 +1,24 @@
+import { createUtility, backgroundColor, colors, backgroundClip, backgroundClipConfig, groupHover, groupFocus, groupActive, groupVisited } from "../../src";
+
+const bg = createUtility()
+  .use(backgroundColor(colors))
+  .use(backgroundClip(backgroundClipConfig))
+  .init();
+
+const utilities = [bg.blue[500], bg.clip.content];
+
+test("groupHover", () => {
+  expect(groupHover(...utilities)).toMatchSnapshot();
+});
+
+test("groupFocus", () => {
+  expect(groupFocus(...utilities)).toMatchSnapshot();
+});
+
+test("groupActive", () => {
+  expect(groupActive(...utilities)).toMatchSnapshot();
+});
+
+test("groupVisited", () => {
+  expect(groupVisited(...utilities)).toMatchSnapshot();
+});
