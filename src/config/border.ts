99adeaf9654d55c9ac870d5720c71e7b404ce2f9
voorjaar <felitas@zoho.com>
2022-07-11 18:51:37 +0800

    add border config

---
+++ src/config/border.ts
@@ -0,0 +1,28 @@
+export const borderRadiusConfig = {
+  default: "0.25rem",
+  none: "0px",
+  sm: "0.125rem",
+  md: "0.375rem",
+  lg: "0.5rem",
+  xl: "0.75rem",
+  xxl: "1rem",
+  xxxl: "1.5rem",
+  1: "100%",
+  full: "9999px",
+};
+
+export const borderStyleConfig = {
+  solid: "solid",
+  dashed: "dashed",
+  dotted: "dotted",
+  double: "double",
+  none: "none",
+};
+
+export const borderWidthConfig = {
+  default: "1px",
+  0: "0px",
+  2: "2px",
+  4: "4px",
+  8: "8px",
+};
