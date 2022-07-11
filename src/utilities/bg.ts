e018a16b6dee14643041d0414b72e5e901ea0fd9
voorjaar <felitas@zoho.com>
2022-07-11 19:00:34 +0800

    use color handler

--- src/utilities/bg.ts
+++ src/utilities/bg.ts
@@ -1,5 +1,6 @@
-import { BgColorStyle, ObjectEntry, StyleBuilder, StyleObject } from "../types";
-import { hasKey, isNumber, useProxy } from "../utils";
+import { StyleObject } from "../types";
+import { isNumber } from "../utils";
+import { ColorOpacityProxy, ColorProxy, createColorHandler, createStaticHandler } from "./handler";
 
 // TODO: support call bg as a function via apply proxy
 // TODO: maybe support delete api, like delete bg.red[500]??
@@ -12,104 +13,36 @@ export function backgroundGeneric () {
         $: {[key: string]: StyleObject}
     }
 
+    const build = (value: string) => ({
+      css: {
+        backgroundColor: value,
+      },
+    });
+
     const handler: RealType = (key: string) => {
       if (isNumber(key)) {
-        return handleSingleColor("#" + (+key).toString(16));
+        return build("#" + (+key).toString(16));
       }
-      return handleSingleColor(key);
+      return build(key);
     };
 
     return handler as unknown as ProxyType;
 }
 
-export function backgroundColor<T extends object> (colors: T) {
-    type BgColorProxy<T> = {
-        [key in keyof T]: (T[key] extends object ? {[k in keyof T[key]]: BgColorStyle} : BgColorStyle) & {
-            [key: string]: BgColorStyle
-        }
-    }
-    type RealType = (key: string) => object | BgColorStyle | undefined;
-    type ProxyType = (key: string) => BgColorProxy<T> | undefined;
-
-    const handler: RealType = (key: string) => {
-      if (hasKey(colors, key)) {
-        const value = colors[key];
-        if (typeof value === "string") {
-          const singleColor = handleSingleColor(value);
-          if (singleColor) return singleColor;
-        }
-        if (typeof value === "object") {
-          const child = value as unknown as {[key: string]: string};
-          return useProxy(prop => {
-            if (hasKey(child, prop)) return handleSingleColor(child[prop]);
-          });
-        }
-      }
-    };
-    return handler as ProxyType;
+export function backgroundColor<T extends object>(colors: T): (key: string) => ColorOpacityProxy<T> | undefined;
+export function backgroundColor<T extends object>(colors: T, withOpacity: true | undefined): (key: string) => ColorOpacityProxy<T> | undefined;
+export function backgroundColor<T extends object>(colors: T, withOpacity: true | undefined, opacityName: string): (key: string) => ColorOpacityProxy<T> | undefined;
+export function backgroundColor<T extends object>(colors: T, withOpacity: false): (key: string) => ColorProxy<T> | undefined;
+export function backgroundColor<T extends object> (colors: T, withOpacity = true, opacityName = "--w-bg-opacity") {
+  if (!withOpacity) return createColorHandler(colors, "backgroundColor");
+  return createColorHandler(colors, "backgroundColor", opacityName);
 }
 
 export function backgroundAttachment<T extends object> (attachments: T) {
-    type RealType = (key: string) => StyleObject | undefined;
-    type ProxyType = (key: string) => ObjectEntry<T> | undefined;
-
-    const build: StyleBuilder = (value) => {
-      return {
-        css: {
-          backgroundAttachment: value,
-        },
-      };
-    };
-
-    const handler: RealType = (key) => {
-      if (hasKey(attachments, key)) {
-        const value = attachments[key];
-        if (typeof value === "string") {
-          return build(value);
-        }
-      }
-    };
-    return handler as ProxyType;
+  return createStaticHandler(attachments, "backgroundAttachment");
 }
 
-export function backgroundClip<T extends object> (clips: T, key = "clip") {
-    type RealType = (key: string) => object | undefined;
-    type ProxyType = (key: string) => { clip: ObjectEntry<T> };
-
-    const build: StyleBuilder = (value) => {
-      return {
-        css: {
-          webkitBackgroundClip: value,
-          backgroundClip: value,
-        },
-      };
-    };
-
-    const handler: RealType = (prop) => {
-      if (prop === key) {
-        return useProxy(clip => {
-          if (hasKey(clips, clip)) {
-            const value = clips[clip];
-            if (typeof value === "string") return build(value);
-          }
-        });
-      }
-    };
-
-    return handler as ProxyType;
-}
-
-function handleSingleColor (value: string): BgColorStyle {
-  return {
-    css: {
-      backgroundColor: value,
-    },
-    opacity: (op) => {
-      return {
-        css: {
-          backgroundColor: value,
-        },
-      };
-    },
-  };
+export function backgroundClip<T extends object, K extends string = "clip"> (clips: T, key?: K) {
+  if (!key) key = "clip" as K;
+  return createStaticHandler(clips, ["backgroundClip", "webkitBackgroundClip"], key);
 }
