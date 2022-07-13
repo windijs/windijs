fd1d77be3d72498c7c865fddc39bbcbfd67e4709
voorjaar <felitas@zoho.com>
2022-07-13 11:10:19 +0800

    new typography config

---
+++ src/config/typography.ts
@@ -0,0 +1,111 @@
+export const fontFamilyConfig = {
+  sans: [
+    "ui-sans-serif",
+    "system-ui",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "\"Segoe UI\"",
+    "Roboto",
+    "\"Helvetica Neue\"",
+    "Arial",
+    "\"Noto Sans\"",
+    "sans-serif",
+    "\"Apple Color Emoji\"",
+    "\"Segoe UI Emoji\"",
+    "\"Segoe UI Symbol\"",
+    "\"Noto Color Emoji\"",
+  ],
+  serif: [
+    "ui-serif",
+    "Georgia",
+    "Cambria",
+    "\"Times New Roman\"",
+    "Times",
+    "serif",
+  ],
+  mono: [
+    "ui-monospace",
+    "SFMono-Regular",
+    "Menlo",
+    "Monaco",
+    "Consolas",
+    "\"Liberation Mono\"",
+    "\"Courier New\"",
+    "monospace",
+  ],
+};
+
+export const fontSizeConfig = {
+  xs: ["0.75rem", { lineHeight: "1rem" }],
+  sm: ["0.875rem", { lineHeight: "1.25rem" }],
+  base: ["1rem", { lineHeight: "1.5rem" }],
+  lg: ["1.125rem", { lineHeight: "1.75rem" }],
+  xl: ["1.25rem", { lineHeight: "1.75rem" }],
+  "2xl": ["1.5rem", { lineHeight: "2rem" }],
+  "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+  "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+  "5xl": ["3rem", { lineHeight: "1" }],
+  "6xl": ["3.75rem", { lineHeight: "1" }],
+  "7xl": ["4.5rem", { lineHeight: "1" }],
+  "8xl": ["6rem", { lineHeight: "1" }],
+  "9xl": ["8rem", { lineHeight: "1" }],
+};
+
+export const fontWeightConfig = {
+  thin: "100",
+  extralight: "200",
+  light: "300",
+  normal: "400",
+  medium: "500",
+  semibold: "600",
+  bold: "700",
+  extrabold: "800",
+  black: "900",
+};
+
+export const hyphensConfig = {
+  auto: "auto",
+  manual: "manual",
+  none: "none",
+};
+
+export const letterSpacingConfig = {
+  tighter: "-0.05em",
+  tight: "-0.025em",
+  normal: "0em",
+  wide: "0.025em",
+  wider: "0.05em",
+  widest: "0.1em",
+};
+
+export const lineHeightConfig = {
+  none: "1",
+  tight: "1.25",
+  snug: "1.375",
+  normal: "1.5",
+  relaxed: "1.625",
+  loose: "2",
+  3: ".75rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  7: "1.75rem",
+  8: "2rem",
+  9: "2.25rem",
+  10: "2.5rem",
+};
+
+export const tabSizeConfig = {
+  DEFAULT: "4",
+  0: "0",
+  2: "2",
+  4: "4",
+  8: "8",
+};
+
+export const textAlignmentConfig = {
+  left: "left",
+  center: "center",
+  right: "right",
+  justify: "justify",
+};
