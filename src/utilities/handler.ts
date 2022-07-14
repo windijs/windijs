a6a3cfe0c97f3d9ed6c2855c23936c512cefcf2a
voorjaar <felitas@zoho.com>
2022-07-14 11:02:28 +0800

    extract color/static build function

--- src/utilities/handler.ts
+++ src/utilities/handler.ts
@@ -1,5 +1,5 @@
-import { CSSObject, ObjectEntry, StyleObject, StyleProperties, UtilityMeta } from "../types";
-import { calcRgba, hasKey, parenWrap, sliceColor, useProxy } from "../utils";
+import { CSSObject, NestedProxy, ObjectEntry, StyleObject, StyleProperties, StyleProxy, UtilityMeta } from "../types";
+import { buildStatic, buildColor, hasKey, useProxy } from "../utils";
 
 /* Static Handler */
 
@@ -11,25 +11,16 @@ export function createStaticHandler<T extends object, K extends string> (statics
   type ProxyType = (uid: string, prop: string) => ObjectEntry<T> | undefined;
   type KeyProxyType = (uid: string, prop: string) => Record<K, ObjectEntry<T>>;
 
-  const build = (uid: string, prop: string, value: string) => {
-    const css: CSSObject = {};
-    if (Array.isArray(property)) {
-      property.forEach(p => (css[p] = value));
-    } else {
-      css[property] = value;
-    }
-    return { css, meta: { uid, type: "static", props: [key, prop].filter(p => p != null) } } as StyleObject;
-  };
-
   const handler = (uid: string, prop: string) => {
+    const meta: UtilityMeta = { uid, type: "static", props: key ? [key, prop] : [prop] };
     if (handleDefault) {
       if (prop === "meta") return { uid, type: "static" };
       // @ts-ignore, generate default css
-      if (prop === "css" && "DEFAULT" in statics) return build(uid, prop, statics.DEFAULT).css;
+      if (prop === "css" && "DEFAULT" in statics) return buildStatic(property, statics.DEFAULT, meta).css;
     }
     if (hasKey(statics, prop)) {
       const value = statics[prop];
-      if (typeof value === "string") return build(uid, prop, value);
+      if (typeof value === "string") return buildStatic(property, value, meta);
     }
   };
 
@@ -47,71 +38,43 @@ export function createStaticHandler<T extends object, K extends string> (statics
 
 /* Color Handler  */
 
-export interface ColorOpacityObject extends StyleObject {
+type ColorOpacityProxy<T> = NestedProxy<T, StyleObject & {
   opacity: (op: number) => StyleObject
-}
+}>;
 
-export type ColorOpacityProxy<T> = {
-  [key in keyof T]: (T[key] extends object ? { [k in keyof T[key]]: ColorOpacityObject } : ColorOpacityObject) & {
-    [key: string]: ColorOpacityObject
-  }
-}
+export type ColorHandler =
+  (<T extends object> (colors: T, withOpacity?: boolean, opacityName?: string) => (uid: string, prop: string) => ColorOpacityProxy<T> | undefined) &
+  (<T extends object> (colors: T, withOpacity: true | undefined, opacityName?: string) => (uid: string, prop: string) => ColorOpacityProxy<T> | undefined) &
+  (<T extends object> (colors: T, withOpacity: false, opacityName?: string) => (uid: string, prop: string) => StyleProxy<T> | undefined);
 
-export type ColorProxy<T> = {
-  [key in keyof T]: (T[key] extends object ? { [k in keyof T[key]]: StyleObject } : StyleObject) & {
-    [key: string]: StyleObject
-  }
-}
-
-export function createColorHandler<T extends object> (colors: T, colorProperty: StyleProperties): (uid: string, prop: string) => ColorProxy<T> | undefined;
+export function createColorHandler<T extends object> (colors: T, colorProperty: StyleProperties, colorOpacityProperty?: string): (uid: string, prop: string) => StyleProxy<T> | undefined;
 export function createColorHandler<T extends object> (colors: T, colorProperty: StyleProperties, colorOpacityProperty: string): (uid: string, prop: string) => ColorOpacityProxy<T> | undefined;
 export function createColorHandler<T extends object> (colors: T, colorProperty: StyleProperties, colorOpacityProperty?: string) {
-  function build (uid: string, p1: string, p2: string | undefined, value: string): ColorOpacityObject | StyleObject {
-    let css: CSSObject = { [colorProperty]: value };
-    const meta: UtilityMeta = { type: "color", uid, props: [p1, p2].filter(i => i != null) as string[] };
-
-    if (colorOpacityProperty != null) {
-      if (value.startsWith("#")) {
-        const [r, g, b, a] = calcRgba(value);
-        css = {
-          [colorProperty]: parenWrap("rgba", [r, g, b, parenWrap("var", colorOpacityProperty)].join(", ")),
-          [colorOpacityProperty]: a.toString(),
-        };
-      } else if (/^(rgb|hwb|hsl)/.test(value)) {
-        const values = sliceColor(value);
-        const alpha = values[3] ?? "1";
-        css = {
-          [colorProperty]: value.startsWith("hwb") ? parenWrap("hwb", values.slice(0, 3).join(" ") + " / " + parenWrap("var", colorOpacityProperty)) : parenWrap(value.startsWith("hsl") ? "hsla" : "rgba", [...values.slice(0, 3), parenWrap("var", colorOpacityProperty)].join(", ")),
-          [colorOpacityProperty]: alpha,
-        };
-      }
-      return {
-        css,
-        meta,
-        opacity (op) {
-          const css: CSSObject = { ...this.css };
-          css[colorOpacityProperty] = (op / 100).toString();
-          meta.props!.push(parenWrap("opacity", op.toString()));
-          return { css, meta };
-        },
-      };
+  function handleNested<C extends object> (uid: string, colors: C, p: string, ps: string[] = []): StyleObject | undefined {
+    if (hasKey(colors, p)) {
+      const value = colors[p];
+      if (typeof value === "string") return buildColor(colorProperty, colorOpacityProperty, value, { type: "color", uid, props: [...ps, p] });
+      if (typeof value === "object") return useProxy(p2 => handleNested(uid, value as unknown as object, p2, [...ps, p])) as StyleObject;
     }
-    return { css, meta };
   }
 
-  return (uid: string, p1: string) => {
-    if (hasKey(colors, p1)) {
-      const value = colors[p1];
-      if (typeof value === "string") {
-        const color = build(uid, p1, undefined, value);
-        if (color) return color;
-      }
-      if (typeof value === "object") {
-        const children = value as unknown as { [key: string]: string };
-        return useProxy(p2 => {
-          if (hasKey(children, p2)) return build(uid, p1, p2, children[p2]);
-        });
-      }
-    }
-  };
+  return (uid: string, p: string) => handleNested(uid, colors, p) as StyleProxy<T> | ColorOpacityProxy<T> | undefined;
+}
+
+type handleColor = <T extends object>(handle: typeof createColorHandler, colors: T, withOpacity?: boolean, opacityName?: string) => ((uid: string, prop: string) => StyleProxy<T> | undefined);
+
+export function useColorHandler (f: handleColor) {
+  return ((colors, withOpacity, opacityName) => f(createColorHandler, colors, withOpacity, opacityName)) as ColorHandler;
+}
+
+type StaticHandler = <T extends object> (statics: T, k?: undefined) => (uid: string, prop: string) => ObjectEntry<T> | undefined;
+type handleStatic = <T extends object> (handle: typeof createStaticHandler, statics: T) => (uid: string, prop: string) => ObjectEntry<T> | undefined;
+type StaticHandlerWithKey<DEFAULT_KEY extends string> = <T extends object, K extends string = DEFAULT_KEY>(statics: T, key?: K) => (uid: string, prop: string) => Record<K, ObjectEntry<T>>
+type handleStaticWithKey = <T extends object, K extends string> (handle: typeof createStaticHandler, statics: T, key: K) => (uid: string, prop: string) => Record<K, ObjectEntry<T>>;
+
+export function useStaticHandler <K extends string> (k: K, f: handleStaticWithKey): StaticHandlerWithKey<K>;
+export function useStaticHandler (f: handleStatic): StaticHandler;
+export function useStaticHandler (keyOrF: string | Function, f2?: Function) {
+  if (typeof keyOrF === "string") return <T> (statics: T, key?: string) => f2!(createStaticHandler, statics, key ?? keyOrF);
+  return <T> (statics: T) => keyOrF(createStaticHandler, statics);
 }
