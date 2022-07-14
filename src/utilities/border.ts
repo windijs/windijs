26b7a2c2ba4ca52315844b9358301b9355466995
voorjaar <felitas@zoho.com>
2022-07-14 11:01:27 +0800

    use color/static handler in border

--- src/utilities/border.ts
+++ src/utilities/border.ts
@@ -1,28 +1,22 @@
 import { StyleProperties } from "../types";
-import { ColorOpacityProxy, ColorProxy, createColorHandler, createStaticHandler } from "./handler";
+import { useColorHandler, useStaticHandler } from "./handler";
 
-export function borderRadius<T extends object> (radius: T) {
-  return createStaticHandler(radius, "borderRadius", undefined, true);
-}
+export const borderRadius = useStaticHandler((handle, radius) =>
+  handle(radius, "borderRadius", undefined, true),
+);
 
-export function borderWidth<T extends object> (width: T) {
-  return createStaticHandler(width, "borderWidth", undefined, true);
-}
+export const borderWidth = useStaticHandler((handle, width) =>
+  handle(width, "borderWidth", undefined, true),
+);
 
-export function borderOpacity<T extends object, K extends string = "opacity"> (opacity: T, key?: K) {
-  if (!key) key = "opacity" as K;
-  return createStaticHandler(opacity, "--w-border-opacity" as StyleProperties, key);
-}
+export const borderOpacity = useStaticHandler("opacity", (handle, opacity, key) =>
+  handle(opacity, "--w-border-opacity" as StyleProperties, key),
+);
 
-export function borderStyle<T extends object> (styles: T) {
-  return createStaticHandler(styles, "borderStyle");
-}
+export const borderStyle = useStaticHandler((handle, styles) =>
+  handle(styles, "borderStyle"),
+);
 
-export function borderColor<T extends object> (colors: T): (uid: string, prop: string) => ColorOpacityProxy<T> | undefined;
-export function borderColor<T extends object> (colors: T, withOpacity: true | undefined): (uid: string, prop: string) => ColorOpacityProxy<T> | undefined;
-export function borderColor<T extends object> (colors: T, withOpacity: true | undefined, opacityName: string): (uid: string, prop: string) => ColorOpacityProxy<T> | undefined;
-export function borderColor<T extends object> (colors: T, withOpacity: false): (uid: string, prop: string) => ColorProxy<T> | undefined;
-export function borderColor<T extends object> (colors: T, withOpacity = true, opacityName = "--w-border-opacity") {
-  if (!withOpacity) return createColorHandler(colors, "borderColor");
-  return createColorHandler(colors, "borderColor", opacityName);
-}
+export const borderColor = useColorHandler((handle, colors, withOpacity = true, opacityName = "--w-border-opacity") =>
+  handle(colors, "borderColor", withOpacity ? opacityName : undefined),
+);
