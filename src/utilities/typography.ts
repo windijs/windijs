81fe70bd9dbb233e36609aed0e6f13e235cdb7e2
voorjaar <felitas@zoho.com>
2022-07-15 13:20:51 +0800

    use css handler in typography

--- src/utilities/typography.ts
+++ src/utilities/typography.ts
@@ -1,81 +1,47 @@
-import { CSSObject, StyleHandler, StyleObject, StyleProperties } from "../types";
-import { hasKey } from "../utils";
-import { useStaticHandler, useColorHandler } from "./handler";
+import { StyleProperties } from "../types";
+import { buildFontSize } from "../utils";
+import { useStaticHandler, useColorHandler, useCSSHandler } from "./handler";
 
 export const fontFamily = useStaticHandler((handle, fonts) => {
   const cssFonts = {} as {[key in keyof typeof fonts]: string};
   for (const [key, value] of Object.entries(fonts)) {
     cssFonts[key as keyof typeof fonts] = Array.isArray(value) ? value.join(",") : value as string;
   }
-  return handle(cssFonts, "fontFamily");
+  return handle(cssFonts as typeof fonts, "fontFamily");
 });
 
-export function fontSize<T extends object> (sizes: T) {
-  const build = (uid: string, prop: string, fontSize: string, lineHeight?: string, others?: { [key in StyleProperties]: string }) => {
-    let css: CSSObject = { fontSize };
-    if (lineHeight != null) css.lineHeight = lineHeight;
-    if (others != null) css = { ...css, ...others } as CSSObject;
-
-    return { css, meta: { type: "static", uid, props: [prop] } } as StyleObject;
-  };
-
-  return ((uid, prop) => {
-    if (hasKey(sizes, prop)) {
-      const value = sizes[prop];
-      if (typeof value === "string") return build(uid, prop, value);
-      if (Array.isArray(value) && value[0]) {
-        if (value[1] == null || typeof value[1] === "string") return build(uid, prop, value[0], value[1]);
-        if (typeof value[1] === "object" && value[1] != null) return build(uid, prop, value[0], undefined, value[1]);
-      }
+export const fontSize = useStaticHandler((handle, sizes) =>
+  handle(sizes, (value, meta) => {
+    if (typeof value === "string") return buildFontSize(value, meta);
+    if (Array.isArray(value) && value[0]) {
+      if (value[1] == null || typeof value[1] === "string") return buildFontSize(value[0], meta, value[1]);
+      if (typeof value[1] === "object" && value[1] != null) return buildFontSize(value[0], meta, undefined, value[1]);
     }
-  }) as StyleHandler<T>;
-}
+  }),
+);
 
 /* Font Smoothing */
 
-export const antialiased: StyleObject & { auto: StyleObject } = {
-  css: {
-    "-webkit-font-smoothing": "antialiased",
-    "-moz-osx-font-smoothing": "grayscale",
+export const fontStyle = useCSSHandler(css => ({
+  italic: {
+    ...css({
+      fontStyle: "italic",
+    }),
+    not: css({
+      fontStyle: "normal",
+    }),
   },
-  meta: {
-    type: "static",
-    uid: "antialiased",
-  },
-  auto: {
-    css: {
+  antialiased: {
+    ...css({
+      "-webkit-font-smoothing": "antialiased",
+      "-moz-osx-font-smoothing": "grayscale",
+    }),
+    auto: css({
       "-webkit-font-smoothing": "auto",
       "-moz-osx-font-smoothing": "auto",
-    },
-    meta: {
-      type: "static",
-      uid: "antialiased",
-      props: ["auto"],
-    },
+    }),
   },
-};
-
-/* Font Style */
-
-export const italic: StyleObject & { not: StyleObject } = {
-  css: {
-    fontStyle: "italic",
-  },
-  meta: {
-    type: "static",
-    uid: "italic",
-  },
-  not: {
-    css: {
-      fontStyle: "normal",
-    },
-    meta: {
-      type: "static",
-      uid: "italic",
-      props: ["not"],
-    },
-  },
-};
+}));
 
 export const fontWeight = useStaticHandler((handle, weights) =>
   handle(weights, "fontWeight"),
