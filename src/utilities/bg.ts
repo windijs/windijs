fb5d6b531ecb59210cb3c05a1cf0e4b123067a07
voorjaar <felitas@zoho.com>
2022-07-15 13:18:01 +0800

    use generic handler for bg

--- src/utilities/bg.ts
+++ src/utilities/bg.ts
@@ -1,37 +1,20 @@
-import { StyleObject } from "../types";
+import { CSSObject, StyleObject } from "../types";
 import { isNumber } from "../utils";
-import { useColorHandler, useStaticHandler } from "./handler";
+import { useColorHandler, useGenericHandler, useStaticHandler } from "./handler";
 
 // TODO: support call bg as a function via apply proxy
 // TODO: maybe support delete api, like delete bg.red[500]??
 
-export function backgroundGeneric () {
-    type ProxyType = (uid: string, prop: string) => {
-        [key: string]: StyleObject
-    } & {
-        $: {[key: string]: StyleObject}
-    }
+export const backgroundGeneric = useGenericHandler<{ [key: string]: StyleObject } & { $: {[key: string]: StyleObject} }>(prop => {
+  const build = (value: string) => ({
+    backgroundColor: value,
+  } as CSSObject);
 
-    const build = (uid: string, prop: string, value: string) => ({
-      css: {
-        backgroundColor: value,
-      },
-      meta: {
-        uid,
-        type: "generic",
-        props: [prop],
-      },
-    }) as StyleObject;
-
-    const handler = (uid: string, prop: string) => {
-      if (isNumber(prop)) {
-        return build(uid, prop, "#" + (+prop).toString(16));
-      }
-      return build(uid, prop, prop);
-    };
-
-    return handler as unknown as ProxyType;
-}
+  if (isNumber(prop)) {
+    return build("#" + (+prop).toString(16));
+  }
+  return build(prop);
+});
 
 export const backgroundColor = useColorHandler((handle, colors, withOpacity = true, opacityName = "--w-bg-opacity") =>
   handle(colors, "backgroundColor", withOpacity ? opacityName : undefined),
@@ -41,6 +24,6 @@ export const backgroundAttachment = useStaticHandler((handle, attachments) =>
   handle(attachments, "backgroundAttachment"),
 );
 
-export const backgroundClip = useStaticHandler("clip", (handle, clips, key) =>
-  handle(clips, ["backgroundClip", "webkitBackgroundClip"], key),
+export const backgroundClip = useStaticHandler("clip", (handle, clips, trigger) =>
+  handle(clips, ["backgroundClip", "webkitBackgroundClip"], trigger),
 );
