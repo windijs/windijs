4bdcded2ea754fcda01f65af102e166e9afe53cf
voorjaar <felitas@zoho.com>
2022-07-15 13:16:57 +0800

    update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,40 +10,52 @@ export type CSSObject = CSSDict & {[key: string]: CSSObject | string}
 
 export type NumberDict = { [key: number]: string };
 
+export type MetaType = "css" | "static" | "color" | "generic";
+
 export type UtilityMeta = {
   uid: string;
-  type: "static" | "color" | "generic";
+  type: MetaType;
   props?: string[];
   variants?: string[];
 };
 
 export interface StyleObject {
-    /** @internal */
-    css: CSSObject;
-    /** @internal */
-    meta?: UtilityMeta;
+  /** @internal */
+  css: CSSObject;
+  /** @internal */
+  meta?: UtilityMeta;
 }
 
-export type StyleEntry<T> = Omit<{ [key in keyof T]: StyleObject }, "meta" | "DEFAULT">
+export type CSSEntry = (css: CSSObject) => StyleObject;
+
+export type SafeEntry<T> = Omit<T, "meta" | "DEFAULT" | "css">;
+
+export type StyleEntry<T> = SafeEntry<{ [key in keyof T]: StyleObject }>
 
 export type Handler<R> = (uid: string, prop: string) => R;
 
 export type StyleHandler<T> = Handler<StyleEntry<T> | undefined>;
 
-export type DefaultedStyleHandler<T> = Handler<StyleEntry<T> & { css: CSSObject } | undefined>;
+// export type DefaultedStyleHandler<T> = Handler<StyleEntry<T> & { css: CSSObject } | undefined>;
 
-export type KeyedStyleHandler<T, K extends string> = Handler<Record<K, StyleEntry<T>>>;
+// export type KeyedStyleHandler<T, K extends string> = Handler<Record<K, StyleEntry<T>>>;
 
-export type KeyedDefaultedStyleHandler<T, K extends string> = Handler<Record<K, StyleEntry<T> & { css: CSSObject }>>;
+// export type KeyedDefaultedStyleHandler<T, K extends string> = Handler<Record<K, StyleEntry<T> & { css: CSSObject }>>;
 
 export type VariantBuilder = (...utilities: StyleObject[]) => StyleObject;
 
-export type NestedProxy<T, O> = {
-  [key in keyof T]: (T[key] extends object ? {
-    [k in keyof T[key]]: (T[key][k] extends object ? NestedProxy<T[key][k], O> : O)
-  } : O);
-}
+export type NestedProxy<T, O> = SafeEntry<{
+  [key in keyof T]: (T[key] extends object ? T[key] extends Array<unknown> ? O : SafeEntry<{
+    [k in keyof T[key]]: (T[key][k] extends object ? T[key][k] extends Array<unknown> ? O : NestedProxy<T[key][k], O> : O)
+  }> : O);
+}>;
 
 export type StyleProxy<T> = NestedProxy<T, StyleObject>;
 
 export type StyleProxyHandler<T> = Handler<StyleProxy<T> | undefined>
+
+export type DefaultedStyleProxyHandler<T> = Handler<StyleProxy<T> & { css: CSSObject }>;
+
+export type KeyedStyleProxyHandler<T, K extends string> = Handler<Record<K, StyleProxy<T>>>;
+
+export type KeyedDefaultedStyleProxyHandler<T, K extends string> = Handler<Record<K, StyleProxy<T> & { css: CSSObject }>>;
