34f136fb96f1b262cb28d86eabf873a162f3ab54
voorjaar <felitas@zoho.com>
2022-07-15 22:55:45 +0800

    update config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   transform: { "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin" },
+  moduleFileExtensions: ["js", "mjs", "cjs", "jsx", "ts", "d.ts", "tsx", "json", "node"],
   collectCoverageFrom: [
     "src/{!(tsconfig.json),}.ts",
     "src/**/*.ts",
