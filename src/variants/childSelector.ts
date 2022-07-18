854173dd142b693d46c43ed37e81fbcd69855d17
voorjaar <felitas@zoho.com>
2022-07-18 23:24:28 +0800

    add docs for child selector

--- src/variants/childSelector.ts
+++ src/variants/childSelector.ts
@@ -1,8 +1,27 @@
 import { VariantBuilder } from "../types";
 import { useVariant } from "./base";
 
+/**
+ * Targets svg nodes.
+ */
 export const svg: VariantBuilder = (...utilities) => useVariant("& svg", utilities);
+
+/**
+ * Target all nodes.
+ */
 export const all: VariantBuilder = (...utilities) => useVariant("& *", utilities);
+
+/**
+ * Targets children nodes.
+ */
 export const children: VariantBuilder = (...utilities) => useVariant("& > *", utilities);
+
+/**
+ * Targets siblings nodes.
+ */
 export const siblings: VariantBuilder = (...utilities) => useVariant("& ~ *", utilities);
+
+/**
+ * Targets first sibling node.
+ */
 export const sibling: VariantBuilder = (...utilities) => useVariant("& + *", utilities);
