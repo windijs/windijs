28397a41c80f087d6b5864d3ee60ef9f68ec8662
voorjaar <felitas@zoho.com>
2022-07-18 23:24:35 +0800

    add docs for parent selector

--- src/variants/parentSelector.ts
+++ src/variants/parentSelector.ts
@@ -1,7 +1,22 @@
 import { VariantBuilder } from "../types";
 import { useVariant } from "./base";
 
+/**
+ * Targets an element when a marked parent matches the hover pseudo-class.
+ */
 export const groupHover: VariantBuilder = (...utilities) => useVariant(".group:hover &", utilities);
+
+/**
+ * Targets an element when a marked parent matches the focus pseudo-class.
+ */
 export const groupFocus: VariantBuilder = (...utilities) => useVariant(".group:focus &", utilities);
+
+/**
+ * Targets an element when a marked parent matches the active pseudo-class.
+ */
 export const groupActive: VariantBuilder = (...utilities) => useVariant(".group:active &", utilities);
+
+/**
+ * Targets an element when a marked parent matches the visited pseudo-class.
+ */
 export const groupVisited: VariantBuilder = (...utilities) => useVariant(".group:visited &", utilities);
