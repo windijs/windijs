cc0b3dcc628bccad09b61fdf1cda3b47518ddff0
voorjaar <felitas@zoho.com>
2022-07-24 18:14:16 +0800

    test build tools

---
+++ tests/helpers/build.test.ts
@@ -0,0 +1,65 @@
+import { atomic, atomicNamer, backgroundColor, borderRadius, borderRadiusConfig, borderWidth, borderWidthConfig, colors, createUtility, fontSize, fontSizeConfig, hashNamer, sm, unify, useNamer } from "../../src";
+
+const text = createUtility("text")
+  .use(fontSize(fontSizeConfig))
+  .init();
+
+const bg = createUtility("bg")
+  .use(backgroundColor(colors, false))
+  .init();
+
+const rounded = createUtility("rounded")
+  .use(borderRadius(borderRadiusConfig))
+  .init();
+
+const border = createUtility("border")
+  .use(borderWidth(borderWidthConfig))
+  .init();
+
+test("unify with utilities", () => {
+  expect(unify(".test", bg.blue[500], text.lg, sm(border, rounded, bg.blue[400]))).toMatchSnapshot();
+
+  expect(unify(".test2", bg.blue[500], text.lg, [rounded.md, border[2]], sm(border, rounded, bg.blue[400]))).toMatchSnapshot();
+});
+
+test("unify with utility object", () => {
+  expect(unify({
+    ".test": [bg.blue[500], text.lg],
+    ".button": [bg.rose[500], rounded, border],
+  })).toMatchSnapshot();
+
+  expect(unify({
+    ".test": [bg.blue[500], text.lg],
+    ".button": [bg.rose[500], rounded, border],
+  }, {
+    p: [text.lg],
+  })).toMatchSnapshot("a");
+
+  expect(unify({
+    ".test": [bg.blue[500], text.lg],
+    ".button": [bg.rose[500], rounded, border],
+  }, {
+    p: text.lg,
+  })).toMatchSnapshot("a");
+});
+
+test("atomic", () => {
+  expect(atomic(bg.blue[500], text.lg, sm(border, rounded, bg.blue[300]))).toMatchSnapshot();
+});
+
+test("atomic with atomicNamer", () => {
+  useNamer(atomicNamer);
+
+  expect(atomic(bg.blue[500], text.lg, sm(border, rounded, bg.blue[300]))).toMatchSnapshot();
+});
+
+test("atomic with hashNamer", () => {
+  useNamer(hashNamer);
+
+  expect(atomic(bg.blue[500], text.lg, sm(border, rounded, bg.blue[300]))).toMatchSnapshot();
+});
+
+test("atomic with empty variant", () => {
+  expect(atomic(sm())).toEqual("");
+  expect(atomic()).toEqual("");
+});
