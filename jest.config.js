6f9d0020d09658cc811a8c93fdaadd9c30f2271b
voorjaar <felitas@zoho.com>
2022-08-02 11:49:43 +0800

    use paths mapper

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   transform: { "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin" },
+  moduleNameMapper: Object.fromEntries(["index", "config", "helpers", "plugins", "preflight", "types", "utilities", "variants", "utils"].map(i => [[`^${i}((/.*)|$)`], [`<rootDir>/src/${i}$1`]])),
   moduleFileExtensions: ["js", "mjs", "cjs", "jsx", "ts", "d.ts", "tsx", "json", "node"],
   collectCoverageFrom: [
     "src/{!(tsconfig.json),}.ts",
