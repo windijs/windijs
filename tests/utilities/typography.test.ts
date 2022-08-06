53c4738f3d2d01e2485e9579a2c5813306f2fb5e
voorjaar <felitas@zoho.com>
2022-08-06 12:36:16 +0800

    rewrite typography utilities

--- tests/utilities/typography.test.ts
+++ tests/utilities/typography.test.ts
@@ -1,11 +1,9 @@
-import { fontFamily, fontSize, fontStyle } from "utilities/typography";
-import { fontFamilyConfig, fontSizeConfig } from "config/typography";
-
-import { createUtility } from "index";
+import { configHandler, createUtility, cssHandler, fontFamilyHandler, fontSizeHandler, guard, meld, prop } from "index";
+import { fontFamilyConfig, fontSizeConfig, fontStyleConfig, fontWeightConfig, hyphensConfig, letterSpacingConfig, lineHeightConfig, tabSizeConfig, textAlignmentConfig } from "config/typography";
 
 test("Font Family", () => {
   const font = createUtility("font")
-    .use(fontFamily(fontFamilyConfig))
+    .use(fontFamilyHandler(fontFamilyConfig))
     .init();
   expect(font.mono.css).toMatchSnapshot();
   expect(font.sans.css).toMatchSnapshot();
@@ -14,7 +12,7 @@ test("Font Family", () => {
 
 test("Font Size", () => {
   const text = createUtility("text")
-    .use(fontSize(fontSizeConfig))
+    .use(fontSizeHandler(fontSizeConfig))
     .init();
   expect(text.xs.css).toMatchSnapshot();
   expect(text["2xl"].css).toMatchSnapshot();
@@ -24,7 +22,7 @@ test("Font Size", () => {
 
 test("Font Size With Different Config", () => {
   const text = createUtility("text")
-    .use(fontSize({
+    .use(fontSizeHandler({
       xs: ["0.75rem", "1rem"],
       sm: ["0.875rem"],
       md: ["1rem", { lineHeight: "1.25rem", letterSpacing: "1px" }],
@@ -36,14 +34,65 @@ test("Font Size With Different Config", () => {
 });
 
 test("Font Style and Font Smoothing", () => {
-  const { italic, antialiased } = createUtility("font").use(fontStyle()).init();
-  expect(italic.not.css).toMatchSnapshot();
-  expect(italic.css).toMatchSnapshot();
-  expect(antialiased.auto.css).toMatchSnapshot();
-  expect(antialiased.css).toMatchSnapshot();
+  const font = createUtility("font")
+    .use(configHandler(fontStyleConfig, "fontStyle"))
+    .case("antialiased", meld(
+      cssHandler({
+        "-webkit-font-smoothing": "antialiased",
+        "-moz-osx-font-smoothing": "grayscale",
+      }),
+      guard("auto", cssHandler({
+        "-webkit-font-smoothing": "auto",
+        "-moz-osx-font-smoothing": "auto",
+      })),
+    ))
+    .init();
 
-  expect(italic.meta).toMatchSnapshot();
-  expect(italic.not.meta).toMatchSnapshot();
-  expect(antialiased.meta).toMatchSnapshot();
-  expect(antialiased.auto.meta).toMatchSnapshot();
+  expect(font.italic.css).toMatchSnapshot();
+  expect(font.normal.css).toMatchSnapshot();
+  expect(font.antialiased.css).toMatchSnapshot();
+  expect(font.antialiased.auto.css).toMatchSnapshot();
+});
+
+test("Font Weight", () => {
+  const font = createUtility("font").use(configHandler(fontWeightConfig, "fontWeight")).init();
+
+  expect(font.bold.css).toMatchSnapshot();
+  expect(font.medium.css).toMatchSnapshot();
+});
+
+test("Hyphens", () => {
+  const hyphens = createUtility("hyphens").use(configHandler(hyphensConfig, [prop`-webkit-hyphens`, prop`-ms-hyphens`, "hyphens"])).init();
+
+  expect(hyphens.manual.css).toMatchSnapshot();
+  expect(hyphens.auto.css).toMatchSnapshot();
+});
+
+test("Letter Spacing", () => {
+  const tracking = createUtility("tracking").use(configHandler(letterSpacingConfig, "letterSpacing")).init();
+
+  expect(tracking.tight.css).toMatchSnapshot();
+  expect(tracking.wider.css).toMatchSnapshot();
+});
+
+test("Line Height", () => {
+  const leading = createUtility("leading").use(configHandler(lineHeightConfig, "lineHeight")).init();
+
+  expect(leading[4].css).toMatchSnapshot();
+  expect(leading.normal.css).toMatchSnapshot();
+  expect(leading.relaxed.css).toMatchSnapshot();
+});
+
+test("Tab Size", () => {
+  const tab = createUtility("tab").use(configHandler(tabSizeConfig, [prop`-moz-tab-size`, prop`-o-tab-size`, "tabSize"])).init();
+
+  expect(tab[4].css).toMatchSnapshot();
+  expect(tab[2].css).toMatchSnapshot();
+});
+
+test("Text Alignment", () => {
+  const text = createUtility("text").use(configHandler(textAlignmentConfig, "textAlign")).init();
+
+  expect(text.left.css).toMatchSnapshot();
+  expect(text.justify.css).toMatchSnapshot();
 });
