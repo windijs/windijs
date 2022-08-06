d274ef512c71838c3550731614173af60818cf25
voorjaar <felitas@zoho.com>
2022-08-06 09:34:36 +0800

    new flex config

---
+++ src/config/flex.ts
@@ -0,0 +1,35 @@
+export const flexDirectionConfig = {
+  row: {
+    DEFAULT: "row",
+    reverse: "row-reverse",
+  },
+  col: {
+    DEFAULT: "column",
+    reverse: "column-reverse",
+  },
+};
+
+export const flexWrapConfig = {
+  wrap: {
+    DEFAULT: "wrap",
+    reverse: "wrap-reverse",
+  },
+  nowrap: "nowrap",
+};
+
+export const flexStretchConfig = {
+  1: "1 1 0%",
+  auto: "1 1 auto",
+  initial: "0 1 auto",
+  none: "none",
+};
+
+export const flexGrowConfig = {
+  DEFAULT: "1",
+  0: "0",
+};
+
+export const flexShrinkConfig = {
+  DEFAULT: "1",
+  0: "0",
+};
