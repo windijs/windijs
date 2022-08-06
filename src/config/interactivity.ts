307fdbe8fb27fc546627796e7fef2bf15de80a7e
voorjaar <felitas@zoho.com>
2022-08-06 09:35:04 +0800

    new interactivity config

---
+++ src/config/interactivity.ts
@@ -0,0 +1,36 @@
+export const touchActionConfig = {
+  auto: "auto",
+  none: "none",
+  pan: {
+    x: "pan-x",
+    y: "pan-y",
+    left: "pan-left",
+    right: "pan-right",
+    up: "pan-up",
+    down: "pan-down",
+  },
+  "pinch-zoom": "pinch-zoom",
+  manipulation: "manipulation",
+};
+
+export const userSelectConfig = {
+  none: "none",
+  text: "text",
+  all: "all",
+  auto: "auto",
+};
+
+export const tableDisplayConfig = {
+  DEFAULT: "table",
+  inline: "inline-table",
+  caption: "table-caption",
+  cell: "table-cell",
+  row: "table-row",
+  column: "table-column",
+  group: {
+    row: "table-row-group",
+    column: "table-column-group",
+    header: "table-header-group",
+    footer: "table-footer-group",
+  },
+};
