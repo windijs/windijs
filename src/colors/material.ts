b81a603f3816d867e5e27bc553710e7539234cc8
voorjaar <felitas@zoho.com>
2022-08-09 18:48:27 +0800

    add material colors

---
+++ src/colors/material.ts
@@ -0,0 +1,366 @@
+export type MaterialColors = keyof typeof materialColors;
+
+export const materialRed = {
+  50: "#FFEBEE",
+  100: "#FFCDD2",
+  200: "#EF9A9A",
+  300: "#E57373",
+  400: "#EF5350",
+  500: "#F44336",
+  600: "#E53935",
+  700: "#D32F2F",
+  800: "#C62828",
+  900: "#B71C1C",
+  accent: {
+    100: "#FF8A80",
+    200: "#FF5252",
+    400: "#FF1744",
+    700: "#D50000",
+  },
+};
+
+export const materialPink = {
+  50: "#FCE4EC",
+  100: "#F8BBD0",
+  200: "#F48FB1",
+  300: "#F06292",
+  400: "#EC407A",
+  500: "#E91E63",
+  600: "#D81B60",
+  700: "#C2185B",
+  800: "#AD1457",
+  900: "#880E4F",
+  accent: {
+    100: "#FF80AB",
+    200: "#FF4081",
+    400: "#F50057",
+    700: "#C51162",
+  },
+};
+
+export const materialPurple = {
+  50: "#F3E5F5",
+  100: "#E1BEE7",
+  200: "#CE93D8",
+  300: "#BA68C8",
+  400: "#AB47BC",
+  500: "#9C27B0",
+  600: "#8E24AA",
+  700: "#7B1FA2",
+  800: "#6A1B9A",
+  900: "#4A148C",
+  accent: {
+    100: "#EA80FC",
+    200: "#E040FB",
+    400: "#D500F9",
+    700: "#AA00FF",
+  },
+};
+
+export const materialDeepPurple = {
+  50: "#EDE7F6",
+  100: "#D1C4E9",
+  200: "#B39DDB",
+  300: "#9575CD",
+  400: "#7E57C2",
+  500: "#673AB7",
+  600: "#5E35B1",
+  700: "#512DA8",
+  800: "#4527A0",
+  900: "#311B92",
+  accent: {
+    100: "#B388FF",
+    200: "#7C4DFF",
+    400: "#651FFF",
+    700: "#6200EA",
+  },
+};
+
+export const materialIndigo = {
+  50: "#E8EAF6",
+  100: "#C5CAE9",
+  200: "#9FA8DA",
+  300: "#7986CB",
+  400: "#5C6BC0",
+  500: "#3F51B5",
+  600: "#3949AB",
+  700: "#303F9F",
+  800: "#283593",
+  900: "#1A237E",
+  accent: {
+    100: "#8C9EFF",
+    200: "#536DFE",
+    400: "#3D5AFE",
+    700: "#304FFE",
+  },
+};
+
+export const materialBlue = {
+  50: "#E3F2FD",
+  100: "#BBDEFB",
+  200: "#90CAF9",
+  300: "#64B5F6",
+  400: "#42A5F5",
+  500: "#2196F3",
+  600: "#1E88E5",
+  700: "#1976D2",
+  800: "#1565C0",
+  900: "#0D47A1",
+  accent: {
+    100: "#82B1FF",
+    200: "#448AFF",
+    400: "#2979FF",
+    700: "#2962FF",
+  },
+};
+
+export const materialLightBlue = {
+  50: "#E1F5FE",
+  100: "#B3E5FC",
+  200: "#81D4FA",
+  300: "#4FC3F7",
+  400: "#29B6F6",
+  500: "#03A9F4",
+  600: "#039BE5",
+  700: "#0288D1",
+  800: "#0277BD",
+  900: "#01579B",
+  accent: {
+    100: "#80D8FF",
+    200: "#40C4FF",
+    400: "#00B0FF",
+    700: "#0091EA",
+  },
+};
+
+export const materialCyan = {
+  50: "#E0F7FA",
+  100: "#B2EBF2",
+  200: "#80DEEA",
+  300: "#4DD0E1",
+  400: "#26C6DA",
+  500: "#00BCD4",
+  600: "#00ACC1",
+  700: "#0097A7",
+  800: "#00838F",
+  900: "#006064",
+  accent: {
+    100: "#84FFFF",
+    200: "#18FFFF",
+    400: "#00E5FF",
+    700: "#00B8D4",
+  },
+};
+
+export const materialTeal = {
+  50: "#E0F2F1",
+  100: "#B2DFDB",
+  200: "#80CBC4",
+  300: "#4DB6AC",
+  400: "#26A69A",
+  500: "#009688",
+  600: "#00897B",
+  700: "#00796B",
+  800: "#00695C",
+  900: "#004D40",
+  accent: {
+    100: "#A7FFEB",
+    200: "#64FFDA",
+    400: "#1DE9B6",
+    700: "#00BFA5",
+  },
+};
+
+export const materialGreen = {
+  50: "#E8F5E9",
+  100: "#C8E6C9",
+  200: "#A5D6A7",
+  300: "#81C784",
+  400: "#66BB6A",
+  500: "#4CAF50",
+  600: "#43A047",
+  700: "#388E3C",
+  800: "#2E7D32",
+  900: "#1B5E20",
+  accent: {
+    100: "#B9F6CA",
+    200: "#69F0AE",
+    400: "#00E676",
+    700: "#00C853",
+  },
+};
+
+export const materialLightGreen = {
+  50: "#F1F8E9",
+  100: "#DCEDC8",
+  200: "#C5E1A5",
+  300: "#AED581",
+  400: "#9CCC65",
+  500: "#8BC34A",
+  600: "#7CB342",
+  700: "#689F38",
+  800: "#558B2F",
+  900: "#33691E",
+  accent: {
+    100: "#CCFF90",
+    200: "#B2FF59",
+    400: "#76FF03",
+    700: "#64DD17",
+  },
+};
+
+export const materialLime = {
+  50: "#F9FBE7",
+  100: "#F0F4C3",
+  200: "#E6EE9C",
+  300: "#DCE775",
+  400: "#D4E157",
+  500: "#CDDC39",
+  600: "#C0CA33",
+  700: "#AFB42B",
+  800: "#9E9D24",
+  900: "#827717",
+  accent: {
+    100: "#F4FF81",
+    200: "#EEFF41",
+    400: "#C6FF00",
+    700: "#AEEA00",
+  },
+};
+
+export const materialYellow = {
+  50: "#FFFDE7",
+  100: "#FFF9C4",
+  200: "#FFF59D",
+  300: "#FFF176",
+  400: "#FFEE58",
+  500: "#FFEB3B",
+  600: "#FDD835",
+  700: "#FBC02D",
+  800: "#F9A825",
+  900: "#F57F17",
+  accent: {
+    100: "#FFFF8D",
+    200: "#FFFF00",
+    400: "#FFEA00",
+    700: "#FFD600",
+  },
+};
+
+export const materialAmber = {
+  50: "#FFF8E1",
+  100: "#FFECB3",
+  200: "#FFE082",
+  300: "#FFD54F",
+  400: "#FFCA28",
+  500: "#FFC107",
+  600: "#FFB300",
+  700: "#FFA000",
+  800: "#FF8F00",
+  900: "#FF6F00",
+  accent: {
+    100: "#FFE57F",
+    200: "#FFD740",
+    400: "#FFC400",
+    700: "#FFAB00",
+  },
+};
+
+export const materialOrange = {
+  50: "#FFF3E0",
+  100: "#FFE0B2",
+  200: "#FFCC80",
+  300: "#FFB74D",
+  400: "#FFA726",
+  500: "#FF9800",
+  600: "#FB8C00",
+  700: "#F57C00",
+  800: "#EF6C00",
+  900: "#E65100",
+  accent: {
+    100: "#FFD180",
+    200: "#FFAB40",
+    400: "#FF9100",
+    700: "#FF6D00",
+  },
+};
+
+export const materialDeepOrange = {
+  50: "#FBE9E7",
+  100: "#FFCCBC",
+  200: "#FFAB91",
+  300: "#FF8A65",
+  400: "#FF7043",
+  500: "#FF5722",
+  600: "#F4511E",
+  700: "#E64A19",
+  800: "#D84315",
+  900: "#BF360C",
+  accent: {
+    100: "#FF9E80",
+    200: "#FF6E40",
+    400: "#FF3D00",
+    700: "#DD2C00",
+  },
+};
+
+export const materialBrown = {
+  50: "#EFEBE9",
+  100: "#D7CCC8",
+  200: "#BCAAA4",
+  300: "#A1887F",
+  400: "#8D6E63",
+  500: "#795548",
+  600: "#6D4C41",
+  700: "#5D4037",
+  800: "#4E342E",
+  900: "#3E2723",
+};
+
+export const materialGray = {
+  50: "#FAFAFA",
+  100: "#F5F5F5",
+  200: "#EEEEEE",
+  300: "#E0E0E0",
+  400: "#BDBDBD",
+  500: "#9E9E9E",
+  600: "#757575",
+  700: "#616161",
+  800: "#424242",
+  900: "#212121",
+};
+
+export const materialBlueGray = {
+  50: "#ECEFF1",
+  100: "#CFD8DC",
+  200: "#B0BEC5",
+  300: "#90A4AE",
+  400: "#78909C",
+  500: "#607D8B",
+  600: "#546E7A",
+  700: "#455A64",
+  800: "#37474F",
+  900: "#263238",
+};
+
+export const materialColors = {
+  red: materialRed,
+  pink: materialPink,
+  purple: materialPurple,
+  deepPurple: materialDeepPurple,
+  indigo: materialIndigo,
+  blue: materialBlue,
+  lightBlue: materialLightBlue,
+  cyan: materialCyan,
+  teal: materialTeal,
+  green: materialGreen,
+  lightGreen: materialLightGreen,
+  lime: materialLime,
+  yellow: materialYellow,
+  amber: materialAmber,
+  orange: materialOrange,
+  deepOrange: materialDeepOrange,
+  brown: materialBrown,
+  gray: materialGray,
+  blueGray: materialBlueGray,
+};
