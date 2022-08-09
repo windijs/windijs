de6bd86ee2e688884e4dcb588a108a8cc6287a0e
voorjaar <felitas@zoho.com>
2022-08-09 18:49:07 +0800

    add apple colors

---
+++ src/colors/apple.ts
@@ -0,0 +1,258 @@
+export const iOSLightColors = {
+  red: "#ff3b30",
+  orange: "#ff9500",
+  yellow: "#ffcc00",
+  green: "#34c759",
+  mint: "#00c7be",
+  teal: "#30b0c7",
+  cyan: "#32ade6",
+  blue: "#007aff",
+  indigo: "#5856d6",
+  purple: "#af52de",
+  pink: "#ff2d55",
+  brown: "#a2845e",
+  gray: "#8e8e93",
+  gray2: "#aeaeb2",
+  gray3: "#c7c7cc",
+  gray4: "#d1d1d6",
+  gray5: "#e5e5ea",
+  gray6: "#f2f2f7",
+};
+
+export const iOSDarkColors = {
+  red: "#ff453a",
+  orange: "#ff9f0a",
+  yellow: "#ffd60a",
+  green: "#32d74b",
+  mint: "#66d4cf",
+  teal: "#40c8e0",
+  cyan: "#64d2ff",
+  blue: "#0a84ff",
+  indigo: "#5e5ce6",
+  purple: "#bf5af2",
+  pink: "#ff375f",
+  brown: "#ac8e68",
+  gray: "#8e8e93",
+  gray2: "#636366",
+  gray3: "#48484a",
+  gray4: "#3a3a3c",
+  gray5: "#2c2c2e",
+  gray6: "#1c1c1e",
+};
+
+export const iOSAccessibleLightColors = {
+  red: "#d70015",
+  orange: "#c93400",
+  yellow: "#b25000",
+  green: "#248a3d",
+  mint: "#0c817b",
+  teal: "#008299",
+  cyan: "#0071a4",
+  blue: "#0040dd",
+  indigo: "#3634a3",
+  purple: "#8944ab",
+  pink: "#d30f45",
+  brown: "#7f6545",
+  gray: "#6c6c70",
+  gray2: "#8e8e93",
+  gray3: "#aeaeb2",
+  gray4: "#bcbcc0",
+  gray5: "#d8d8dc",
+  gray6: "#ebebf0",
+};
+
+export const iOSAccessibleDarkColors = {
+  red: "#ff6961",
+  orange: "#ffb340",
+  yellow: "#ffd426",
+  green: "#30db5b",
+  mint: "#66d4cf",
+  teal: "#5de6ff",
+  cyan: "#70d7ff",
+  blue: "#409cff",
+  indigo: "#7d7aff",
+  purple: "#da8fff",
+  pink: "#ff6482",
+  brown: "#b59469",
+  gray: "#aeaeb2",
+  gray2: "#7c7c80",
+  gray3: "#545456",
+  gray4: "#444446",
+  gray5: "#363638",
+  gray6: "#242426",
+};
+
+export const iOSColors = {
+  light: iOSLightColors,
+  dark: iOSDarkColors,
+};
+
+export const iOSAccessibleColors = {
+  light: iOSAccessibleLightColors,
+  dark: iOSAccessibleDarkColors,
+};
+
+export const macOSLightColors = {
+  red: "#ff3b30",
+  orange: "#ff9500",
+  yellow: "#ffcc00",
+  green: "#34c759",
+  mint: "#00c7be",
+  teal: "#59adc4",
+  cyan: "#55bef0",
+  blue: "#007aff",
+  indigo: "#5856d6",
+  purple: "#af52de",
+  pink: "#ff2d55",
+  brown: "#a2845e",
+  gray: "#8e8e93",
+};
+
+export const macOSDarkColors = {
+  red: "#ff453a",
+  orange: "#ff9f0a",
+  yellow: "#ffd60a",
+  green: "#32d74b",
+  mint: "#66d4cf",
+  teal: "#6ac4dc",
+  cyan: "#5ac8f5",
+  blue: "#0a84ff",
+  indigo: "#5e5ce6",
+  purple: "#bf5af2",
+  pink: "#ff2d55",
+  brown: "#ac8e68",
+  gray: "#98989d",
+};
+
+export const macOSVibrantLightColors = {
+  red: "#ff3126",
+  orange: "#f58b00",
+  yellow: "#f5c200",
+  green: "#1ec337",
+  mint: "#00bdb4",
+  teal: "#2ea7bd",
+  cyan: "#41afdc",
+  blue: "#0070f5",
+  indigo: "#5452cc",
+  purple: "#9f4bc9",
+  pink: "#f5234b",
+  brown: "#987a54",
+  gray: "#848489",
+};
+
+export const macOSVibrantDarkColors = {
+  red: "#ff4f44",
+  orange: "#ffa914",
+  yellow: "#ffe014",
+  green: "#3ce155",
+  mint: "#6ce0db",
+  teal: "#44d4ed",
+  cyan: "#5acdfa",
+  blue: "#148eff",
+  indigo: "#6361f2",
+  purple: "#cc65ff",
+  pink: "#ff4169",
+  brown: "#b69872",
+  gray: "#a2a2a7",
+};
+
+export const macOSAccessibleLightColors = {
+  red: "#d70015",
+  orange: "#c93400",
+  yellow: "#a05a00",
+  green: "#007d1b",
+  mint: "#0c817b",
+  teal: "#008299",
+  cyan: "#0071a4",
+  blue: "#0040dd",
+  indigo: "#3634a3",
+  purple: "#ad44ab",
+  pink: "#d30f45",
+  brown: "#7f6545",
+  gray: "#69696e",
+
+};
+
+export const macOSAccessibleDarkColors = {
+  red: "#ff6961",
+  orange: "#ffb340",
+  yellow: "#ffd426",
+  green: "#31de4b",
+  mint: "#66d4cf",
+  teal: "#5de6ff",
+  cyan: "#70d7ff",
+  blue: "#409cff",
+  indigo: "#7d7aff",
+  purple: "#da8fff",
+  pink: "#ff6482",
+  brown: "#b59469",
+  gray: "#98989d",
+};
+
+export const macOSAccessibleVibrantLightColors = {
+  red: "#c20618",
+  orange: "#ad3a00",
+  yellow: "#925100",
+  green: "#007018",
+  mint: "#0b7570",
+  teal: "#00778c",
+  cyan: "#006796",
+  blue: "#0040dd",
+  indigo: "#3634a3",
+  purple: "#ad44ab",
+  pink: "#c11032",
+  brown: "#775d3b",
+  gray: "#616165",
+};
+
+export const macOSAccessibleVibrantDarkColors = {
+  red: "#ff4136",
+  orange: "#ffb340",
+  yellow: "#ffd426",
+  green: "#31de4b",
+  mint: "#66d4cf",
+  teal: "#5de6ff",
+  cyan: "#70d7ff",
+  blue: "#409cff",
+  indigo: "#7d7aff",
+  purple: "#da8fff",
+  pink: "#ff3a5f",
+  brown: "#b59469",
+  gray: "#98989d",
+};
+
+export const macOSColors = {
+  light: macOSLightColors,
+  dark: macOSDarkColors,
+};
+
+export const macOSVibrantColors = {
+  light: macOSVibrantLightColors,
+  dark: macOSVibrantDarkColors,
+};
+
+export const macOSAccessibleColors = {
+  light: macOSAccessibleLightColors,
+  dark: macOSAccessibleDarkColors,
+};
+
+export const macOSAccessibleVibrantColors = {
+  light: macOSAccessibleVibrantLightColors,
+  dark: macOSAccessibleVibrantDarkColors,
+};
+
+export const watchOSColors = {
+  red: "#ff3b30",
+  orange: "#ff9500",
+  yellow: "#ffe620",
+  green: "#04de71",
+  mint: "#66d4cf",
+  teal: "#6ac4dc",
+  cyan: "#5ac8fa",
+  blue: "#2094fa",
+  indigo: "#787aff",
+  purple: "#bf5af2",
+  pink: "#fa114f",
+  brown: "#ac8e68",
+  gray: "#9ba0aa",
+};
