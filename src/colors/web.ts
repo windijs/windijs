155765cad6751f66df31bab3701a75cac7a2795c
voorjaar <felitas@zoho.com>
2022-08-09 18:48:58 +0800

    add web colors

---
+++ src/colors/web.ts
@@ -0,0 +1,189 @@
+export type WebColors = keyof typeof webColors;
+
+export const webPink = {
+  mediumVioletRed: "#c71585",
+  deepPink: "#ff1493",
+  paleVioletRed: "#db7093",
+  hotPink: "#ff69b4",
+  lightPink: "#ffb6c1",
+  pink: "#ffc0cb",
+};
+
+export const webRed = {
+  darkRed: "#8b0000",
+  red: "#ff0000",
+  firebrick: "#b22222",
+  crimson: "#dc143c",
+  indianRed: "#cd5c5c",
+  lightCoral: "#f08080",
+  salmon: "#fa8072",
+  darkSalmon: "#e9967a",
+  lightSalmon: "#ffa07a",
+};
+
+export const webOrange = {
+  orangeRed: "#ff4500",
+  tomato: "#ff6347",
+  darkOrange: "#ff8c00",
+  coral: "#ff7f50",
+  orange: "#ffa500",
+};
+
+export const webYellow = {
+  darkKhaki: "#bdb76b",
+  gold: "#ffd700",
+  khaki: "#f0e68c",
+  peachPuff: "#ffdab9",
+  yellow: "#ffff00",
+  paleGoldenrod: "#eee8aa",
+  moccasin: "#ffe4b5",
+  papayaWhip: "#ffefd5",
+  lightGoldenrodYellow: "#fafad2",
+  lemonChiffon: "#fffacd",
+  lightYellow: "#ffffe0",
+};
+
+export const webBrown = {
+  maroon: "#800000",
+  brown: "#a52a2a",
+  saddleBrown: "#8b4513",
+  sienna: "#a0522d",
+  chocolate: "#d2691e",
+  darkGoldenrod: "#b8860b",
+  peru: "#cd853f",
+  rosyBrown: "#bc8f8f",
+  goldenrod: "#daa520",
+  sandyBrown: "#f4a460",
+  tan: "#d2b48c",
+  burlywood: "#deb887",
+  wheat: "#f5deb3",
+  navajoWhite: "#ffdead",
+  bisque: "#ffe4c4",
+  blanchedAlmond: "#ffebcd",
+  cornsilk: "#fff8dc",
+};
+
+export const webGreen = {
+  darkGreen: "#006400",
+  green: "#008000",
+  darkOliveGreen: "#556b2f",
+  forestGreen: "#228b22",
+  seaGreen: "#2e8b57",
+  olive: "#808000",
+  oliveDrab: "#6b8e23",
+  mediumSeaGreen: "#3cb371",
+  limeGreen: "#32cd32",
+  lime: "#00ff00",
+  springGreen: "#00ff7f",
+  mediumSpringGreen: "#00fa9a",
+  darkSeaGreen: "#8fbc8f",
+  mediumAquamarine: "#66cdaa",
+  yellowGreen: "#9acd32",
+  lawnGreen: "#7cfc00",
+  chartreuse: "#7fff00",
+  lightGreen: "#90ee90",
+  greenYellow: "#adff2f",
+  paleGreen: "#98fb98",
+};
+
+export const webCyan = {
+  teal: "#008080",
+  darkCyan: "#008b8b",
+  lightSeaGreen: "#20b2aa",
+  cadetBlue: "#5f9ea0",
+  darkTurquoise: "#00ced1",
+  mediumTurquoise: "#48d1cc",
+  turquoise: "#40e0d0",
+  aqua: "#00ffff",
+  cyan: "#00ffff",
+  aquamarine: "#7fffd4",
+  paleTurquoise: "#afeeee",
+  lightCyan: "#e0ffff",
+};
+
+export const webBlue = {
+  navy: "#000080",
+  darkBlue: "#00008b",
+  mediumBlue: "#0000cd",
+  blue: "#0000ff",
+  midnightBlue: "#191970",
+  royalBlue: "#4169e1",
+  steelBlue: "#4682b4",
+  dodgerBlue: "#1e90ff",
+  deepSkyBlue: "#00bfff",
+  cornflowerBlue: "#6495ed",
+  skyBlue: "#87ceeb",
+  lightSkyBlue: "#87cefa",
+  lightSteelBlue: "#b0c4de",
+  lightBlue: "#add8e6",
+  powderBlue: "#b0e0e6",
+};
+
+export const webPurple = {
+  indigo: "#4b0082",
+  purple: "#800080",
+  darkMagenta: "#8b008b",
+  darkViolet: "#9400d3",
+  darkSlateBlue: "#483d8b",
+  blueViolet: "#8a2be2",
+  darkOrchid: "#9932cc",
+  fuchsia: "#ff00ff",
+  magenta: "#ff00ff",
+  slateBlue: "#6a5acd",
+  mediumSlateBlue: "#7b68ee",
+  mediumOrchid: "#ba55d3",
+  mediumPurple: "#9370db",
+  orchid: "#da70d6",
+  violet: "#ee82ee",
+  plum: "#dda0dd",
+  thistle: "#d8bfd8",
+  lavender: "#e6e6fa",
+  rebeccaPurple: "#663399",
+};
+
+export const webWhite = {
+  mistyRose: "#ffe4e1",
+  antiqueWhite: "#faebd7",
+  linen: "#faf0e6",
+  beige: "#f5f5dc",
+  whiteSmoke: "#f5f5f5",
+  lavenderBlush: "#fff0f5",
+  oldLace: "#fdf5e6",
+  aliceBlue: "#f0f8ff",
+  seashell: "#fff5ee",
+  ghostWhite: "#f8f8ff",
+  honeydew: "#f0fff0",
+  floralWhite: "#fffaf0",
+  azure: "#f0ffff",
+  mintCream: "#f5fffa",
+  snow: "#fffafa",
+  ivory: "#fffff0",
+  white: "#ffffff",
+};
+
+export const webBlack = {
+  black: "#000000",
+  darkSlateGray: "#2f4f4f",
+  dimGray: "#696969",
+  slateGray: "#708090",
+  gray: "#808080",
+  lightSlateGray: "#778899",
+  darkGray: "#a9a9a9",
+  silver: "#c0c0c0",
+  lightGray: "#d3d3d3",
+  gainsboro: "#dcdcdc",
+};
+
+export const webColors = {
+  ...webPink,
+  ...webRed,
+  ...webOrange,
+  ...webYellow,
+  ...webBrown,
+  ...webGreen,
+  ...webCyan,
+  ...webBlue,
+  ...webPurple,
+  ...webWhite,
+  ...webBlack,
+};
