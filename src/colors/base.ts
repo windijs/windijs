680912b386c71ac99896b62c38e28dcf23697b6d
voorjaar <felitas@zoho.com>
2022-08-09 18:49:20 +0800

    add base colors

---
+++ src/colors/base.ts
@@ -0,0 +1,9 @@
+export type BaseColors = keyof typeof baseColors;
+
+export const baseColors = {
+  inherit: "inherit",
+  current: "currentColor",
+  transparent: "transparent",
+  black: "#000",
+  white: "#fff",
+};
