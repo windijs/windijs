1d6c3f9da0e0c60204169c20052051beaee7c066
voorjaar <felitas@zoho.com>
2022-08-14 15:14:40 +0800

    add svg config

---
+++ src/config/svg.ts
@@ -0,0 +1,11 @@
+export const strokeLineCapConfig = {
+  auto: "butt",
+  square: "square",
+  round: "round",
+};
+
+export const strokeLineJoinConfig = {
+  auto: "miter",
+  bevel: "bevel",
+  round: "round",
+};
