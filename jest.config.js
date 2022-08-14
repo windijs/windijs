5500ed48c70c3fdab7e9a56f584033b3f9054367
voorjaar <felitas@zoho.com>
2022-08-14 22:51:18 +0800

    setup packages config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   transform: { "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin" },
-  moduleNameMapper: Object.fromEntries(["core", "colors", "config", "helpers", "preflight", "types", "utilities", "variants", "utils"].map(i => [[`^${i}((/.*)|$)`], [`<rootDir>/src/${i}$1`]])),
+  moduleNameMapper: Object.fromEntries(["core", "colors", "config", "helpers", "shared", "preflight", "types", "utilities", "variants"].map(i => [[`^@windi/${i}((/.*)|$)`], [`<rootDir>/packages/${i}$1/src/index`]])),
   moduleFileExtensions: ["js", "mjs", "cjs", "jsx", "ts", "d.ts", "tsx", "json", "node"],
   collectCoverageFrom: [
-    "src/{!(tsconfig.json),}.ts",
-    "src/**/*.ts",
+    "packages/{!(tsconfig.json),}.ts",
+    "packages/**/*.test.ts",
   ],
 };
