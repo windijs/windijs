05306cab190e0376e0fd5fbd3e50dfd05dbc058a
voorjaar <felitas@zoho.com>
2022-08-14 19:01:38 +0800

    update lit example

--- examples/lit/src/my-element.js
+++ examples/lit/src/my-element.js
@@ -2,6 +2,7 @@ import { WindiElement, bg } from "./windi"
 import { css, html } from 'lit'
 
 import litLogo from './assets/lit.svg'
+import { styles } from "./styles.windi"
 
 /**
  * An example element.
@@ -64,76 +65,7 @@ export class MyElement extends WindiElement {
   }
 
   static get styles() {
-    return css`
-      :host {
-        max-width: 1280px;
-        margin: 0 auto;
-        padding: 2rem;
-        text-align: center;
-      }
-
-      .logo {
-        height: 6em;
-        padding: 1.5em;
-        will-change: filter;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.lit:hover {
-        filter: drop-shadow(0 0 2em #325cffaa);
-      }
-
-      .card {
-        padding: 2em;
-      }
-
-      .read-the-docs {
-        color: #888;
-      }
-
-      a {
-        font-weight: 500;
-        color: #646cff;
-        text-decoration: inherit;
-      }
-      a:hover {
-        color: #535bf2;
-      }
-
-      h1 {
-        font-size: 3.2em;
-        line-height: 1.1;
-      }
-
-      button {
-        border-radius: 8px;
-        border: 1px solid transparent;
-        padding: 0.6em 1.2em;
-        font-size: 1em;
-        font-weight: 500;
-        font-family: inherit;
-        background-color: #1a1a1a;
-        cursor: pointer;
-        transition: border-color 0.25s;
-      }
-      button:hover {
-        border-color: #646cff;
-      }
-      button:focus,
-      button:focus-visible {
-        outline: 4px auto -webkit-focus-ring-color;
-      }
-
-      @media (prefers-color-scheme: light) {
-        a:hover {
-          color: #747bff;
-        }
-        button {
-          background-color: #f9f9f9;
-        }
-      }
-    `
+    return styles
   }
 }
 
---
+++ examples/lit/src/styles.windi.js
@@ -0,0 +1,73 @@
+/// <reference types="windijs/global" />
+
+import { $, em, focus, focusVisible, hover, light } from "windijs";
+
+import { css as litCSS } from "lit";
+
+const buttonFocused = [outlineWidth.px[4], outlineStyle.auto, outlineColor["-webkit-focus-ring-color"]];
+
+// TODO: fix variant unify problem
+// TODO: @fontface?
+
+const button = $.Button(
+  borderRadius.px[8],
+  borderWidth.px[1],
+  borderStyle.solid,
+  borderColor.transparent,
+  paddingTop.em[0.6],
+  paddingBottom.em[0.6],
+  paddingLeft.em[1.2],
+  paddingRight.em[1.2],
+  fontSize.em[1],
+  fontWeight[500],
+  fontFamily.inherit,
+  backgroundColor.rgb(26, 26, 26),
+  cursor.pointer,
+  transition["border-color 0.25s"],
+  hover(borderColor.rgb(100, 108, 255)),
+  focus(buttonFocused),
+  focusVisible(buttonFocused),
+  light(backgroundColor.rgb(249, 249, 249))
+)
+
+const logo = $.logo(
+  height.em[6],
+  padding.em[1.5],
+  willChange.filter,
+  hover(filter.dropShadow(0, 0, em[2], "#646cffaa")),
+  // css("&.lit", hover(filter.dropShadow(0, 0, em[2], "#325cffaa")))
+)
+
+const host = $.Host(
+  maxWidth.px[1280],
+  marginTop[0],
+  marginBottom[0],
+  marginLeft.auto,
+  marginRight.auto,
+  padding.rem[2],
+  textAlign.center
+)
+
+const others = $({
+  ".card": padding.em[2],
+  ".read-the-docs": color.rgb(136, 136 ,136),
+  a: [
+    fontWeight[500],
+    color.rgb(100, 108, 255),
+    textDecoration.inherit,
+    hover(color.rgb(83, 91, 242)),
+    light(color.rgb(116, 123, 255))
+  ],
+  h1: [
+    fontSize.em[3.2],
+    lineHeight[1.1]
+  ]
+})
+
+export const styles = [
+  litCSS([host]),
+  litCSS([others]),
+  litCSS([button]),
+  litCSS([logo]),
+]
+
