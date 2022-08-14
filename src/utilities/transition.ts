39c4716d161ff8c7a48cb2858973ca773b5f8762
voorjaar <felitas@zoho.com>
2022-08-14 17:59:19 +0800

    extract transition utilities

---
+++ src/utilities/transition.ts
@@ -0,0 +1,33 @@
+import { buildTransition, configHandler, createUtility } from "core";
+import { css, prop } from "helpers";
+import { transitionDelayConfig, transitionDurationConfig, transitionPropertyConfig, transitionTimingFunctionConfig } from "config";
+
+export const transition = createUtility("transition").use(configHandler(transitionPropertyConfig, (v) => {
+  const timef = transitionTimingFunctionConfig.DEFAULT;
+  const dur = transitionDurationConfig.DEFAULT;
+  if (v === "none") return css({ transitionProperty: "none" });
+  if (typeof v === "string") {
+    return css({
+      transitionProperty: v,
+      "-webkit-transition-timing-function": timef,
+      "-o-transition-timing-function": timef,
+      transitionTimingFunction: timef,
+      "-webkit-transition-duration": dur,
+      "-o-transition-duration": dur,
+      transitionDuration: dur,
+    });
+  };
+}))
+  .init(buildTransition);
+
+export const duration = createUtility("duration")
+  .use(configHandler(transitionDurationConfig, [prop`-webkit-transition-duration`, prop`-o-transition-duration`, "transitionDuration"]))
+  .init();
+
+export const ease = createUtility("ease")
+  .use(configHandler(transitionTimingFunctionConfig, [prop`-webkit-transition-timing-function`, prop`-o-transition-timing-function`, "transitionTimingFunction"]))
+  .init();
+
+export const delay = createUtility("delay")
+  .use(configHandler(transitionDelayConfig, [prop`-webkit-transition-delay`, prop`-o-transition-delay`, "transitionDelay"]))
+  .init();
--- tests/utilities/transition.test.ts
+++ tests/utilities/transition.test.ts
@@ -1,28 +1,11 @@
-import { buildTransition, configHandler, createUtility, msHandler } from "core";
-import { css, prop } from "helpers";
-import { transitionDelayConfig, transitionDurationConfig, transitionPropertyConfig, transitionTimingFunctionConfig } from "config";
+import { createUtility, msHandler } from "core";
+import { delay, duration, ease, transition } from "utilities";
 
 import type { StyleObject } from "types";
+import { prop } from "helpers";
+import { transitionPropertyConfig as property } from "config";
 
 test("Transition", () => {
-  const transition = createUtility("transition").use(configHandler(transitionPropertyConfig, (v) => {
-    const timef = transitionTimingFunctionConfig.DEFAULT;
-    const dur = transitionDurationConfig.DEFAULT;
-    if (v === "none") return css({ transitionProperty: "none" });
-    if (typeof v === "string") {
-      return css({
-        transitionProperty: v,
-        "-webkit-transition-timing-function": timef,
-        "-o-transition-timing-function": timef,
-        transitionTimingFunction: timef,
-        "-webkit-transition-duration": dur,
-        "-o-transition-duration": dur,
-        transitionDuration: dur,
-      });
-    };
-  }))
-    .init(buildTransition);
-
   expect(transition.css).toMatchSnapshot();
   expect(transition.all.css).toMatchSnapshot();
   expect(transition.none.css).toMatchSnapshot();
@@ -30,16 +13,10 @@ test("Transition", () => {
 });
 
 test("Transition As Function", () => {
-  const transition = createUtility("transition").init(buildTransition);
-  const property = transitionPropertyConfig;
-  const duration = createUtility("duration").use(configHandler(transitionDurationConfig, [prop`-webkit-transition-duration`, prop`-o-transition-duration`, "transitionDuration"])).init();
-  const ease = createUtility("ease").use(configHandler(transitionTimingFunctionConfig, [prop`-webkit-transition-timing-function`, prop`-o-transition-timing-function`, "transitionTimingFunction"])).init();
-  const delay = createUtility("delay").use(configHandler(transitionDelayConfig, [prop`-webkit-transition-delay`, prop`-o-transition-delay`, "transitionDelay"])).init();
   expect(transition(property.all, duration[150], ease.linear, delay[100]).css).toMatchSnapshot();
 });
 
 test("Transition Duration", () => {
-  const duration = createUtility("duration").use(configHandler(transitionDurationConfig, [prop`-webkit-transition-duration`, prop`-o-transition-duration`, "transitionDuration"])).init();
   expect(duration[75].css).toMatchSnapshot();
   expect(duration[200].css).toMatchSnapshot();
 });
@@ -53,15 +30,12 @@ test("Transition Duration without config", () => {
 });
 
 test("Transition Timing Function", () => {
-  const ease = createUtility("ease").use(configHandler(transitionTimingFunctionConfig, [prop`-webkit-transition-timing-function`, prop`-o-transition-timing-function`, "transitionTimingFunction"])).init();
-
   expect(ease.linear.css).toMatchSnapshot();
   expect(ease.in.css).toMatchSnapshot();
   expect(ease.inOut.css).toMatchSnapshot();
 });
 
 test("Transition Delay", () => {
-  const delay = createUtility("delay").use(configHandler(transitionDelayConfig, [prop`-webkit-transition-delay`, prop`-o-transition-delay`, "transitionDelay"])).init();
   expect(delay[75].css).toMatchSnapshot();
   expect(delay[200].css).toMatchSnapshot();
 });
