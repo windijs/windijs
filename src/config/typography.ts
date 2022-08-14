4144a0115d99f09e0367c8c91e260f8a5ff71a01
voorjaar <felitas@zoho.com>
2022-08-14 15:15:24 +0800

    update typography config

--- src/config/typography.ts
+++ src/config/typography.ts
@@ -103,9 +103,123 @@ export const tabSizeConfig = {
   8: "8",
 };
 
-export const textAlignmentConfig = {
+export const textAlignConfig = {
   left: "left",
   center: "center",
   right: "right",
   justify: "justify",
+};
+
+export const verticalAlignConfig = {
+  baseline: "baseline",
+  top: "top",
+  middle: "middle",
+  bottom: "bottom",
+  textTop: "text-top",
+  textBottom: "text-bottom",
+};
+
+export const fontVariantNumericConfig = {
+  nums: {
+    normal: "normal",
+    ordinal: "ordinal",
+    slashedZero: "slashed-zero",
+    lining: "lining-nums",
+    oldstyle: "oldstyle-nums",
+    proportional: "proportional-nums",
+    tabular: "tabular-nums",
+  },
+  fractions: {
+    diagonal: "diagonal-fractions",
+    stacked: "stacked-fractions",
+  },
+};
+
+export const textDecorationTypeConfig = {
+  underline: "underline",
+  overline: "overline",
+  linethrough: "line-through",
+  none: "none",
+};
+
+export const textDecorationStyleConfig = {
+  solid: "solid",
+  double: "double",
+  dotted: "dotted",
+  dashed: "dashed",
+  wavy: "wavy",
+};
+
+export const textDecorationThicknessConfig = {
+  auto: "auto",
+  0: "0px",
+  1: "1px",
+  2: "2px",
+  3: "3px",
+  4: "4px",
+  5: "5px",
+  6: "6px",
+  7: "7px",
+  8: "8px",
+};
+
+export const textDecorationOffsetConfig = textDecorationThicknessConfig;
+
+export const textIndentConfig = {
+  DEFAULT: "1.5rem",
+  xs: "0.5rem",
+  sm: "1rem",
+  md: "1.5rem",
+  lg: "2rem",
+  xl: "2.5rem",
+  xxl: "3rem",
+  xxxxl: "4rem",
+};
+
+export const textStrokeWidthConfig = {
+  DEFAULT: "medium",
+  none: "0",
+  sm: "thin",
+  md: "medium",
+  lg: "thick",
+};
+
+export const textShadowConfig = {
+  DEFAULT: "0px 0px 1px rgb(0 0 0 / 20%), 0px 0px 1px rgb(1 0 5 / 10%)",
+  sm: "1px 1px 3px rgb(36 37 47 / 25%)",
+  md: "0px 1px 2px rgb(30 29 39 / 19%), 1px 2px 4px rgb(54 64 147 / 18%)",
+  lg: "3px 3px 6px rgb(0 0 0 / 26%), 0 0 5px rgb(15 3 86 / 22%)",
+  xl: "1px 1px 3px rgb(0 0 0 / 29%), 2px 4px 7px rgb(73 64 125 / 35%)",
+  none: "none",
+};
+
+export const textTransformConfig = {
+  normal: "none",
+  uppercase: "uppercase",
+  lowercase: "lowercase",
+  capitalize: "capitalize",
+};
+
+export const whiteSpaceConfig = {
+  normal: "normal",
+  nowrap: "nowrap",
+  pre: {
+    DEFAULT: "pre",
+    line: "pre-line",
+    wrap: "pre-wrap",
+  },
+};
+
+export const writingModeConfig = {
+  normal: "horizontal-tb",
+  vertical: {
+    right: "vertical-rl",
+    left: "vertical-lr",
+  },
+};
+
+export const writingOrientationConfig = {
+  mixed: "mixed",
+  upright: "upright",
+  sideways: "sideways",
 };
