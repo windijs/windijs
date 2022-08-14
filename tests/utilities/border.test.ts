8ee4e5d4d520e70352cd4afc7df353ae7bea93b2
voorjaar <felitas@zoho.com>
2022-08-14 17:57:28 +0800

    extract border utilities

--- tests/utilities/border.test.ts
+++ tests/utilities/border.test.ts
@@ -1,14 +1,11 @@
-import { baseColors, createUtility, prop, windiColors } from "index";
-import { borderRadiusConfig, borderStyleConfig, borderWidthConfig, opacityConfig, outlineOffsetConfig, ringOffsetConfig } from "config";
-import { buildDivideColor, buildDivideOpacity, buildDivideStyle, buildDivideX, buildDivideY, buildRingWidth, colorHandler, configHandler, cssHandler, divideXReverseHandler, divideYReverseHandler, guard, meld } from "core";
+import { border, divide, outline, ring, rounded } from "utilities";
+import { createUtility, prop } from "index";
 
 import type { PickValue } from "types";
+import { configHandler } from "core";
+import { opacityConfig } from "config";
 
 test("Border Radius", () => {
-  const rounded = createUtility("rounded")
-    .use(configHandler(borderRadiusConfig, "borderRadius"))
-    .init();
-
   expect(rounded.css).toMatchSnapshot();
   expect(rounded.full.css).toMatchSnapshot();
   expect(rounded[1].css).toMatchSnapshot();
@@ -22,9 +19,6 @@ test("Border Radius", () => {
 });
 
 test("Border Style", () => {
-  const border = createUtility("border")
-    .use(configHandler(borderStyleConfig, "borderStyle"))
-    .init();
   expect(border.solid.css).toMatchSnapshot();
   expect(border.dashed.css).toMatchSnapshot();
   expect(border.dotted.css).toMatchSnapshot();
@@ -47,7 +41,6 @@ test("Border With Nested Config", () => {
   const border = createUtility("border")
     .use(borderStyleHandler)
     .init();
-  // @ts-ignore
   expect(border.nested.css).toMatchSnapshot();
   expect(border.solid.css).toMatchSnapshot();
   expect(border.nested.dashed.css).toMatchSnapshot();
@@ -55,9 +48,6 @@ test("Border With Nested Config", () => {
 });
 
 test("Border Opacity", () => {
-  const border = createUtility("border")
-    .case("opacity", configHandler(opacityConfig, prop`--w-border-opacity`))
-    .init();
   expect(border.opacity[0].css).toMatchSnapshot();
   expect(border.opacity[50].css).toMatchSnapshot();
   expect(border.opacity[30].css).toMatchSnapshot();
@@ -95,19 +85,12 @@ test("Border Opacity With Different Trigger", () => {
 });
 
 test("Border Width", () => {
-  const border = createUtility("border")
-    .use(configHandler(borderWidthConfig, "borderWidth"))
-    .init();
   expect(border[0].css).toMatchSnapshot();
   expect(border[4].css).toMatchSnapshot();
   expect(border.css).toMatchSnapshot();
 });
 
 test("Border Color", () => {
-  const border = createUtility("border")
-    .use(colorHandler({ ...baseColors, ...windiColors }, "borderColor", "--w-border-opacity"))
-    .init();
-
   expect(border.current.css).toMatchSnapshot();
   expect(border.white.css).toMatchSnapshot();
   expect(border.blue[700].css).toMatchSnapshot();
@@ -115,17 +98,6 @@ test("Border Color", () => {
 });
 
 test("Divider Width", () => {
-  const divide = createUtility("divide")
-    .case("x", meld(
-      guard("reverse", divideXReverseHandler()),
-      configHandler(borderWidthConfig, buildDivideX),
-    ))
-    .case("y", meld(
-      guard("reverse", divideYReverseHandler()),
-      configHandler(borderWidthConfig, buildDivideY),
-    ))
-    .init();
-
   expect(divide.x[0].css).toMatchSnapshot();
   expect(divide.x[4].css).toMatchSnapshot();
   expect(divide.x.css).toMatchSnapshot();
@@ -137,10 +109,6 @@ test("Divider Width", () => {
 });
 
 test("Divider Color", () => {
-  const divide = createUtility("divide")
-    .use(colorHandler({ ...baseColors, ...windiColors }, buildDivideColor))
-    .init();
-
   expect(divide.current.css).toMatchSnapshot();
   expect(divide.white.css).toMatchSnapshot();
   expect(divide.blue[700].css).toMatchSnapshot();
@@ -149,19 +117,11 @@ test("Divider Color", () => {
 });
 
 test("Divider Opacity", () => {
-  const divide = createUtility("divide")
-    .case("opacity", configHandler(opacityConfig, buildDivideOpacity))
-    .init();
-
   expect(divide.opacity[0].css).toMatchSnapshot();
   expect(divide.opacity[50].css).toMatchSnapshot();
 });
 
 test("Divider Style", () => {
-  const divide = createUtility("divide")
-    .use(configHandler(borderStyleConfig, buildDivideStyle))
-    .init();
-
   expect(divide.solid.css).toMatchSnapshot();
   expect(divide.dashed.css).toMatchSnapshot();
   expect(divide.dotted.css).toMatchSnapshot();
@@ -170,35 +130,21 @@ test("Divider Style", () => {
 });
 
 test("Outline Color", () => {
-  const outline = createUtility("outline")
-    .use(colorHandler(windiColors, "outlineColor", "--w-outline-opacity"))
-    .init();
-
   expect(outline.amber[300].css).toMatchSnapshot();
   expect(outline.blue[400].opacity(10).css).toMatchSnapshot();
 });
 
 test("Outline Width", () => {
-  const outline = createUtility("outline")
-    .use(configHandler(borderWidthConfig, "outlineWidth"))
-    .init();
   expect(outline[0].css).toMatchSnapshot();
   expect(outline[4].css).toMatchSnapshot();
   expect(outline.css).toMatchSnapshot();
 });
 
 test("Outline Offset", () => {
-  const outline = createUtility("outline")
-    .case("offset", configHandler(outlineOffsetConfig, "outlineOffset"))
-    .init();
-
   expect(outline.offset[2].css).toMatchSnapshot();
 });
 
 test("Outline Style", () => {
-  const outline = createUtility("outline")
-    .use(configHandler(borderStyleConfig, "outlineStyle"))
-    .init();
   expect(outline.solid.css).toMatchSnapshot();
   expect(outline.dashed.css).toMatchSnapshot();
   expect(outline.dotted.css).toMatchSnapshot();
@@ -207,37 +153,21 @@ test("Outline Style", () => {
 });
 
 test("Outline Opacity", () => {
-  const outline = createUtility("outline")
-    .case("opacity", configHandler(opacityConfig, prop`--w-outline-opacity`))
-    .init();
-
   expect(outline.opacity[0].css).toMatchSnapshot();
   expect(outline.opacity[50].css).toMatchSnapshot();
 });
 
 test("Ring Color", () => {
-  const ring = createUtility("ring")
-    .use(colorHandler(windiColors, prop`--w-ring-color`, "--w-ring-opacity"))
-    .init();
-
   expect(ring.amber[300].css).toMatchSnapshot();
   expect(ring.blue[400].opacity(10).css).toMatchSnapshot();
 });
 
 test("Ring Opacity", () => {
-  const ring = createUtility("ring")
-    .case("opacity", configHandler(opacityConfig, prop`--w-ring-opacity`))
-    .init();
-
   expect(ring.opacity[0].css).toMatchSnapshot();
   expect(ring.opacity[50].css).toMatchSnapshot();
 });
 
 test("Ring Width", () => {
-  const ring = createUtility("ring")
-    .use(configHandler(borderWidthConfig, buildRingWidth))
-    .case("inset", cssHandler({ "--w-ring-inset": "inset" }))
-    .init();
   expect(ring[0].css).toMatchSnapshot();
   expect(ring[4].css).toMatchSnapshot();
   expect(ring.inset.css).toMatchSnapshot();
@@ -245,14 +175,6 @@ test("Ring Width", () => {
 });
 
 test("Ring Offset", () => {
-  const ring = createUtility("ring")
-    .case("offset", meld(
-      colorHandler(windiColors, prop`--w-ring-offset-color`, "--w-ring-offset-opacity"),
-      configHandler(ringOffsetConfig, prop`--w-ring-offset-width`),
-      guard("opacity", configHandler(opacityConfig, prop`--w-ring-offset-opacity`)),
-    ))
-    .init();
-
   expect(ring.offset[2].css).toMatchSnapshot();
   expect(ring.offset.cyan[300].css).toMatchSnapshot();
   expect(ring.offset.gray[300].opacity(20).css).toMatchSnapshot();
