c4a15cccd4002316205e09052102b2a43f9b56d8
voorjaar <felitas@zoho.com>
2022-08-14 15:18:12 +0800

    test accessibility

---
+++ tests/utilities/accessibility.test.ts
@@ -0,0 +1,30 @@
+import { createUtility, cssHandler } from "utilities";
+
+test("Screen Readers", () => {
+  const sr = createUtility("sr")
+    .case("only", cssHandler({
+      position: "absolute",
+      width: "1px",
+      height: "1px",
+      padding: "0",
+      margin: "-1px",
+      overflow: "hidden",
+      clip: "rect(0, 0, 0, 0)",
+      whiteSpace: "nowrap",
+      borderWidth: "0",
+    }))
+    .case("normal", cssHandler({
+      position: "static",
+      width: "auto",
+      height: "auto",
+      padding: "0",
+      margin: "0",
+      overflow: "visible",
+      clip: "auto",
+      whiteSpace: "normal",
+    }))
+    .init();
+
+  expect(sr.only.css).toMatchSnapshot();
+  expect(sr.normal.css).toMatchSnapshot();
+});
