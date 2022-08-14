ef78d231bf116a10eb9173d3128b1e0af2e32569
voorjaar <felitas@zoho.com>
2022-08-14 15:12:51 +0800

    add behaviors config

---
+++ src/config/behaviors.ts
@@ -0,0 +1,49 @@
+export const boxDecorationBreakConfig = {
+  clone: "clone",
+  slice: "slice",
+};
+
+export const imageRenderingConfig = {
+  auto: "auto",
+  pixel: "pixelated",
+  edge: "crisp-edges",
+};
+
+export const listStyleTypeConfig = {
+  none: "none",
+  disc: "disc",
+  circle: "circle",
+  square: "square",
+  decimal: "decimal",
+  zeroDecimal: "decimal-leading-zero",
+  greek: "lower-greek",
+  roman: "lower-roman",
+  upperRoman: "upper-roman",
+  alpha: "lower-alpha",
+  upperAlpha: "upper-alpha",
+};
+
+export const listStylePositionConfig = {
+  inside: "inside",
+  outside: "outside",
+};
+
+export const overflowConfig = {
+  auto: "auto",
+  clip: "clip",
+  hidden: "hidden",
+  visible: "visible",
+  scroll: "scroll",
+};
+
+export const overscrollConfig = {
+  auto: "auto",
+  none: "none",
+  contain: "contain",
+};
+
+export const aspectRatioConfig = {
+  auto: "auto",
+  square: "1 / 1",
+  video: "16 / 9",
+};
