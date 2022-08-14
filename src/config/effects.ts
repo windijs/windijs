029cc0968e0eabd06b62c7e67dfb01ffacccee22
voorjaar <felitas@zoho.com>
2022-08-14 15:13:42 +0800

    add effects config

---
+++ src/config/effects.ts
@@ -0,0 +1,31 @@
+export const blendModeConfig = {
+  normal: "normal",
+  multiply: "multiply",
+  screen: "screen",
+  overlay: "overlay",
+  darken: "darken",
+  lighten: "lighten",
+  hardLight: "hard-light",
+  softLight: "soft-light",
+  difference: "difference",
+  exclusion: "exclusion",
+  hue: "hue",
+  saturation: "saturation",
+  color: {
+    DEFAULT: "color",
+    dodge: "color-dodge",
+    burn: "color-burn",
+  },
+  luminosity: "luminosity",
+};
+
+export const boxShadowConfig = {
+  DEFAULT: "0 1px 3px 0 rgb(0 0 0/0.1),0 1px 2px -1px rgb(0 0 0/0.1)",
+  sm: "0 1px 2px 0 rgb(0 0 0/0.05)",
+  md: "0 4px 6px -1px rgb(0 0 0/0.1),0 2px 4px -2px rgb(0 0 0/0.1)",
+  lg: "0 10px 15px -3px rgb(0 0 0/0.1),0 4px 6px -4px rgb(0 0 0/0.1)",
+  xl: "0 20px 25px -5px rgb(0 0 0/0.1),0 8px 10px -6px rgb(0 0 0/0.1)",
+  xxl: "0 25px 50px -12px rgb(0 0 0/0.25)",
+  inner: "inset 0 2px 4px 0 rgb(0 0 0/0.05)",
+  none: "0 0 #0000",
+};
