67a2505e1a314a768ca2bc7bbbfb2f53e633a976
voorjaar <felitas@zoho.com>
2022-08-14 15:13:29 +0800

    add border config

--- src/config/border.ts
+++ src/config/border.ts
@@ -22,7 +22,18 @@ export const borderStyleConfig = {
 export const borderWidthConfig = {
   DEFAULT: "1px",
   0: "0px",
+  1: "1px",
   2: "2px",
   4: "4px",
   8: "8px",
 };
+
+export const outlineOffsetConfig = {
+  0: "0px",
+  1: "1px",
+  2: "2px",
+  4: "4px",
+  8: "8px",
+};
+
+export const ringOffsetConfig = outlineOffsetConfig;
