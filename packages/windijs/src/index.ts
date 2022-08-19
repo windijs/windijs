493bc73cf20adf9b9887b1440539ee8ca8802914
voorjaar <felitas@zoho.com>
2022-08-19 13:20:14 +0800

    update main entry

--- packages/windijs/src/index.ts
+++ packages/windijs/src/index.ts
@@ -1,1 +1,6 @@
 export * from "@windi/core";
+export * from "@windi/config";
+export * from "@windi/colors";
+// export * from "@windi/helpers";
+export * from "@windi/utilities";
+export * from "@windi/variants";
