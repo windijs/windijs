b3cc99138f77a6673e53a108a129b993be10eca6
voorjaar <felitas@zoho.com>
2022-08-22 15:01:30 +0800

    add jsdoc type for jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/** @type { import("@jest/types").Config.InitialOptions } */
 module.exports = {
   transform: { "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin" },
   moduleNameMapper: Object.fromEntries(require("fs").readdirSync("./packages").map(i => [[`^@windi/${i}`], [`<rootDir>/packages/${i}/src`]])),
