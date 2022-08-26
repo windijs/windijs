51ad8a0bb5d74ec369ea9a32baa0474b8a59cb3c
voorjaar <felitas@zoho.com>
2022-08-26 13:10:54 +0800

    fix: transformers entry

--- packages/transformer/src/index.ts
+++ packages/transformer/src/index.ts
@@ -3,4 +3,7 @@ export * from "./utility";
 export * from "./analyzer";
 export * from "./intersection";
 export * from "./fixes";
+export * from "./omit";
+export * from "./updates";
 export * from "./utils";
+export * from "./inject";
