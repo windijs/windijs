b7917b4d9f1b49fd7a996e7321017c88a2f4e8a8
voorjaar <felitas@zoho.com>
2022-08-26 13:09:48 +0800

    feat: new update variable type transformer

---
+++ packages/transformer/src/updates.ts
@@ -0,0 +1,21 @@
+import { Bundle, Node, SourceFile, TransformerFactory, TypeNode, factory, isIdentifier, isVariableDeclaration, visitEachChild, visitNode } from "typescript";
+
+/**
+ * Typescript Transform Plugin.
+ * Transform variable type to another type
+ */
+export function updateVariableType (dict: Record<string, (node: TypeNode) => TypeNode>) {
+  const constsTransformer: TransformerFactory<Bundle | SourceFile> = context => {
+    return sourceFile => {
+      const visitor = (node: Node): Node => {
+        if (isVariableDeclaration(node) && isIdentifier(node.name) && node.type) {
+          const key = node.name.escapedText.toString();
+          if (key in dict) return factory.updateVariableDeclaration(node, node.name, node.exclamationToken, dict[key](node.type), node.initializer);
+        }
+        return visitEachChild(node, visitor, context);
+      };
+      return visitNode(sourceFile, visitor);
+    };
+  };
+  return constsTransformer;
+}
