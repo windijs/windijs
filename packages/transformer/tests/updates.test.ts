21e31dbd35e81a80042d5477a5fd794028eb35c0
voorjaar <felitas@zoho.com>
2022-08-26 13:10:28 +0800

    test: update transformer

---
+++ packages/transformer/tests/updates.test.ts
@@ -0,0 +1,15 @@
+import { updateVariableType, useTransformer } from "../src";
+
+import { factory } from "typescript";
+
+test("Transform omit", () => {
+  const code = `
+  export declare const fill: Omit<{
+    $colors: StyleObject<{}>;
+    "none": StyleObject<{}>;
+  }, "DEFAULT" | "a">;
+  export declare const stroke: 3;
+  `;
+
+  expect(useTransformer(code, updateVariableType({ fill: () => factory.createLiteralTypeNode(factory.createStringLiteral("hello")), stroke: (node) => factory.createTypeReferenceNode("Pick", [factory.createTypeLiteralNode([]), node]) }))).toMatchSnapshot();
+});
