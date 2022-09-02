7ddcda0a19608f8de4fc21b705408c7ad49dab51
voorjaar <felitas@zoho.com>
2022-09-02 13:52:27 +0800

    fix: preact example

--- examples/preact/vite.config.js
+++ examples/preact/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import { vitePlugins } from "@windijs/plugin-utils"
+import windiConfig from './windi.config'
+
+const { plugins, windijs } = vitePlugins.vanilla({
+  config: windiConfig
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()]
+  plugins: [
+    ...plugins,
+    windijs([".jsx"]),
+    preact()
+  ]
 })
