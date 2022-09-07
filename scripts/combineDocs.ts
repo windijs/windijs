c6a658a74656f2d656e3b736528ac790efa307a4
voorjaar <felitas@zoho.com>
2022-09-07 09:21:59 +0800

    fix: onclick sometimes lost direction

--- scripts/combineDocs.ts
+++ scripts/combineDocs.ts
@@ -1,10 +1,15 @@
+/* eslint-disable no-console */
 import { basename, resolve } from "path";
 import { copyFileSync, existsSync, mkdirSync, readFileSync, readdirSync, statSync, writeFileSync } from "fs";
 
+import chalk from "chalk";
+
 /* Api pages is build with typedoc, this is a hack to redirect api link */
-function transformIndex (entry = "./docs/.vitepress/dist/index.html") {
+function transformIndex (entry = "docs/.vitepress/dist/index.html") {
+  console.log(chalk.cyan("\nTransforming '" + entry + "'"));
   let src = readFileSync(entry).toString();
-  src = src.replace("href=\"/api.html\"", "style=\"cursor:pointer;\" onclick=\"document.location.href='/api.html'\"");
+  src = src.replace("href=\"/api.html\"", "href=\"/api.html\" target=\"_blank\" rel=\"noreferrer\"");
+  // "style=\"cursor:pointer;\" onclick=\"document.location.href='/api.html'\"");
   writeFileSync(entry, src);
 }
 
@@ -18,7 +23,8 @@ function copy (item: string, target: string) {
   }
 }
 
-function copyApi (entry = "./api", target = "./docs/.vitepress/dist") {
+function copyApi (entry = "api", target = "docs/.vitepress/dist") {
+  console.log(chalk.cyan("\nCopying API from '" + entry + "' to '" + target + "'"));
   copy(resolve(entry, "modules.html"), resolve(target, "api.html"));
   copy(resolve(entry, "assets"), resolve(target, "assets"));
   readdirSync(entry).filter(i => i !== "assets" && !i.endsWith(".html")).map(i => resolve(entry, i)).map(i => copy(i, resolve(target, basename(i))));
@@ -26,3 +32,4 @@ function copyApi (entry = "./api", target = "./docs/.vitepress/dist") {
 
 transformIndex();
 copyApi();
+console.log(chalk.green("\nDone!"));
