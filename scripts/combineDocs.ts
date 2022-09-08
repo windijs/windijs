045ac904c8cacf7d3c0fdf74af9a08b8042a5f48
voorjaar <felitas@zoho.com>
2022-09-08 13:17:25 +0800

    style: print dots

--- scripts/combineDocs.ts
+++ scripts/combineDocs.ts
@@ -6,7 +6,7 @@ import chalk from "chalk";
 
 /* Api pages is build with typedoc, this is a hack to redirect api link */
 function transformIndex (entry = "docs/.vitepress/dist/index.html") {
-  console.log(chalk.cyan("\nTransforming '" + entry + "'"));
+  console.log(chalk.cyan("\nTransforming '" + entry + "'..."));
   let src = readFileSync(entry).toString();
   src = src.replace("href=\"/api.html\"", "href=\"/api.html\" target=\"_blank\" rel=\"noreferrer\"");
   // "style=\"cursor:pointer;\" onclick=\"document.location.href='/api.html'\"");
@@ -24,7 +24,7 @@ function copy (item: string, target: string) {
 }
 
 function copyApi (entry = "api", target = "docs/.vitepress/dist") {
-  console.log(chalk.cyan("\nCopying API from '" + entry + "' to '" + target + "'"));
+  console.log(chalk.cyan("\nCopying API from '" + entry + "' to '" + target + "'..."));
   copy(resolve(entry, "modules.html"), resolve(target, "api.html"));
   copy(resolve(entry, "assets"), resolve(target, "assets"));
   readdirSync(entry).filter(i => i !== "assets" && !i.endsWith(".html")).map(i => resolve(entry, i)).map(i => copy(i, resolve(target, basename(i))));
