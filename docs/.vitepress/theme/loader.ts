bf40bed23d644cd179dfda7f7a8d4bec9891495c
voorjaar <felitas@zoho.com>
2022-09-08 13:11:48 +0800

    feat: add srr loader

---
+++ docs/.vitepress/theme/loader.ts
@@ -0,0 +1,36 @@
+import { StyleLoader, StyleObject, applyVariant, baseStyleHandler, baseStyleTarget, buildRules, createRules, hashNamer, injectCSS, nameStyle, useNamer, useStyleLoader } from "windijs";
+
+const CSS_LIST: string[] = [];
+const CLASS_LIST: string[] = [];
+
+export function mountCSS () {
+  return injectCSS(CSS_LIST.join("\n"));
+}
+
+export const ssrLoader: StyleLoader = ((css, meta, data) => {
+  const baseStyle = baseStyleTarget(css, meta, data) as StyleObject;
+  const className = nameStyle(baseStyle);
+  const inject = (v: unknown) => {
+    if (!CLASS_LIST.includes(className)) {
+      CLASS_LIST.push(className);
+      CSS_LIST.push(buildRules(createRules(applyVariant(baseStyle), "." + className)));
+    }
+    return v;
+  };
+
+  return new Proxy({
+    [className]: true,
+    ...baseStyle,
+  }, {
+    get (target, prop, receiver) {
+    // for react, svelte...
+      if (prop === "toString") return () => inject(Object.keys(target).join(" "));
+      // for vue
+      if (prop in target) return inject(Reflect.get(target, prop, receiver));
+      return baseStyleHandler(target, prop, receiver);
+    },
+  });
+});
+
+useNamer(hashNamer);
+useStyleLoader(ssrLoader);
