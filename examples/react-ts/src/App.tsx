4c8cfe813697086369cab3bdb325b0c9812c357e
voorjaar <felitas@zoho.com>
2022-09-25 12:02:56 +0800

    style: format example

--- examples/react-ts/src/App.tsx
+++ examples/react-ts/src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
-import type { BootstrapColors } from 'windijs'
-import reactLogo from './assets/react.svg'
-import { useState } from 'react'
+import type { BootstrapColors } from "windijs";
+import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [button, setButtonStyle] = useState([bg.pink[400]])
-  const colors: BootstrapColors[] = ["red", "orange", "yellow", "green", "teal", "blue", "purple", "pink"]
+  const [count, setCount] = useState(0);
+  const [button, setButtonStyle] = useState([bg.pink[400]]);
+  const colors: BootstrapColors[] = ["red", "orange", "yellow", "green", "teal", "blue", "purple", "pink"];
   return (
     <div className="App">
       <div>
@@ -21,21 +21,21 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button className={button.toString()} onClick={() => {
-          setButtonStyle([bg[colors[count % 8]][400]])
-          setCount((count) => count + 1)
-        }}>
+        <button
+          className={button.toString()}
+          onClick={() => {
+            setButtonStyle([bg[colors[count % 8]][400]]);
+            setCount(count => count + 1);
+          }}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
