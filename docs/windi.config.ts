563ecf1a0bdb5d102730a7bb4cfac95850953495
voorjaar <felitas@zoho.com>
2022-09-25 11:54:50 +0800

    ci: update windi config

--- docs/windi.config.ts
+++ docs/windi.config.ts
@@ -1,3 +1,24 @@
-export default {
+import { css, defineConfig } from "windijs";
 
-};
+export default defineConfig({
+  darkMode: "class",
+  utilities: {
+    layout: {
+      hstack: css({
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+      }),
+      vstack: css({
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }),
+    },
+    cursor: {
+      pointer: css({
+        cursor: "pointer",
+      }),
+    },
+  },
+});
