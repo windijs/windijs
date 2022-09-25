45cdc404f1a4e9b9e8d3f8fa63ef13d6ae1025cf
voorjaar <felitas@zoho.com>
2022-09-25 12:01:23 +0800

    refactor: change props and variants in resetMeta

---
+++ .changeset/grumpy-lies-walk.md
@@ -0,0 +1,5 @@
+---
+"@windijs/helpers": patch
+---
+
+refactor: support change props and variants when resetMeta
--- packages/helpers/src/meta.ts
+++ packages/helpers/src/meta.ts
@@ -1,32 +1,32 @@
-import type { MetaType, StyleObject, UtilityMeta } from "./types";
-
 import { SymbolMeta } from "./common";
+
+import type { MetaType, StyleObject, UtilityMeta } from "./types";
 
 let CurrentMeta: UtilityMeta;
 
-export function resetMeta (uid = "css", type: MetaType = "css") {
-  CurrentMeta = { uid, type, props: [], variants: [] };
+export function resetMeta(uid = "css", type: MetaType = "css", props: string[] = [], variants: string[] = []) {
+  CurrentMeta = { uid, type, props, variants };
 }
 
 resetMeta();
 
-export function getMeta () {
+export function getMeta() {
   return CurrentMeta;
 }
 
-export function getUid () {
+export function getUid() {
   return CurrentMeta.uid;
 }
 
-export function pushMetaProp (prop: string) {
+export function pushMetaProp(prop: string) {
   return CurrentMeta.props.push(prop);
 }
 
-export function updateMetaType (type: MetaType) {
+export function updateMetaType(type: MetaType) {
   CurrentMeta.type = type;
 }
 
-export function resetStyleMeta (style: StyleObject, meta: UtilityMeta = CurrentMeta) {
+export function resetStyleMeta(style: StyleObject, meta: UtilityMeta = CurrentMeta) {
   style[SymbolMeta] = meta;
   return style;
 }
