ede18d0571f4202398a443c39241988e59b4839a
voorjaar <felitas@zoho.com>
2022-09-26 00:03:50 +0800

    fix: utilities build

--- rollup.config.js
+++ rollup.config.js
@@ -25,25 +25,25 @@ const resolve = (/** @type {string} */ p) => path.join(packageDir, p);
 // ensure TS checks only once for each build
 let hasTSChecked = false;
 
-function createTS (format) {
+function createTS(format) {
   const plugin = isProduction
     ? ts({
-      check: !hasTSChecked,
-      tsconfig: "tsconfig.json",
-      tsconfigOverride: {
-        compilerOptions: {
-          target: format === "cjs" ? "es2020" : "es2018",
-          sourceMap,
-          declaration: false,
-          declarationMap: false,
+        check: !hasTSChecked,
+        tsconfig: "tsconfig.json",
+        tsconfigOverride: {
+          compilerOptions: {
+            target: format === "cjs" ? "es2020" : "es2018",
+            sourceMap,
+            declaration: false,
+            declarationMap: false,
+          },
+          exclude: ["node_modules/**", "**/tests"],
         },
-        exclude: ["node_modules/**", "**/tests"],
-      },
-    })
+      })
     : sucrase({
-      exclude: ["node_modules/**", "**/tests"],
-      transforms: ["typescript"],
-    });
+        exclude: ["node_modules/**", "**/tests"],
+        transforms: ["typescript"],
+      });
 
   hasTSChecked = true;
   return plugin;
@@ -55,7 +55,7 @@ function createTS (format) {
  * @param {string} name
  * @returns
  */
-function createOutput (format, name) {
+function createOutput(format, name) {
   return {
     cjs: {
       file: resolve(`dist/${name}.js`),
@@ -98,7 +98,7 @@ function createOutput (format, name) {
  * @param {import("rollup").OutputOptions} output
  * @returns {import("rollup").RollupOptions}
  */
-function createConfig (format, name, entry = "index.ts", output = createOutput(format, name)) {
+function createConfig(format, name, entry = "index.ts", output = createOutput(format, name)) {
   if (!output) {
     // eslint-disable-next-line no-console
     console.log(require("chalk").yellow(`invalid format: "${format}"`));
@@ -114,23 +114,21 @@ function createConfig (format, name, entry = "index.ts", output = createOutput(f
 
   const plugins = [createTS(format), commonjs(), nodeResolve()];
 
-  if (process.env.MINIFY || (format === "runtime" && isProduction)) {
-    plugins.push(terser({
-      module: format === "esm",
-      compress: {
-        ecma: 2015,
-        pure_getters: true,
-      },
-      safari10: true,
-    }));
-  }
+  if (process.env.MINIFY || (format === "runtime" && isProduction))
+    plugins.push(
+      terser({
+        module: format === "esm",
+        compress: {
+          ecma: 2015,
+          pure_getters: true,
+        },
+        safari10: true,
+      })
+    );
 
   return defineConfig({
     input: resolve("src/" + entry),
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     plugins,
     output,
     treeshake: {
@@ -141,17 +139,16 @@ function createConfig (format, name, entry = "index.ts", output = createOutput(f
 
 const options = packageFormats.map(format => createConfig(format, name));
 
-if (name === "utilities") {
+if (name === "windijsUtilities")
   // build es modules, good for tree-shaking.
-  options.push(createConfig("es", name, undefined, {
-    dir: resolve("dist/es"),
-    format: "esm",
-    preserveModules: true,
-  }));
-}
+  options.push(
+    createConfig("es", name, undefined, {
+      dir: resolve("dist/es"),
+      format: "esm",
+      preserveModules: true,
+    })
+  );
 
-if (name === "plugin-utils") {
-  options.push(...packageFormats.map(format => createConfig(format, "vite", "vite.ts")));
-}
+if (name === "plugin-utils") options.push(...packageFormats.map(format => createConfig(format, "vite", "vite.ts")));
 
 export default options;
