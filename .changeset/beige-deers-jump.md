b53363dc74427a65fcf3820079076c0519dde637
voorjaar <felitas@zoho.com>
2022-09-26 00:02:19 +0800

    fix: style meta duplicated

---
+++ .changeset/beige-deers-jump.md
@@ -0,0 +1,5 @@
+---
+"@windijs/style": patch
+---
+
+fix: style meta duplicated
--- packages/style/src/handler.ts
+++ packages/style/src/handler.ts
@@ -1,12 +1,207 @@
-import { $in, $var, BuildFunc, CSSDecls, GeneralCSSData, Q, StyleHandler, StyleProxy, abs, attr, blur, brightness, calc, ch, circle, clamp, cm, conicGradient, contrast, counter, counters, css, cubicBezier, deg, dpcm, dpi, dppx, dropShadow, ellipse, em, env, ex, fitContent, fr, getMeta, grad, grayscale, hsl, hsla, hueRotate, hwb, inset, invert, linearGradient, matrix, matrix3d, max, min, minmax, mm, ms, opacity, path, pc, percent, perspective, polygon, pt, pushMetaProp, px, quote, rad, radialGradient, rem, repeat, repeatingConicGradient, repeatingLinearGradient, repeatingRadialGradient, rgb, rgba, rotate, rotate3d, rotateX, rotateY, rotateZ, s, saturate, scale, scale3d, scaleX, scaleY, scaleZ, sepia, sign, skew, skewX, skewY, steps, translate, translate3d, translateX, translateY, translateZ, turn, url, useProxy, vh, vmax, vmin, vw, x } from "@windijs/helpers";
-
 import { handleConfig } from "@windijs/core";
+import {
+  $in,
+  $var,
+  abs,
+  attr,
+  blur,
+  brightness,
+  BuildFunc,
+  calc,
+  ch,
+  circle,
+  clamp,
+  cm,
+  conicGradient,
+  contrast,
+  counter,
+  counters,
+  css,
+  CSSDecls,
+  cubicBezier,
+  deg,
+  dpcm,
+  dpi,
+  dppx,
+  dropShadow,
+  ellipse,
+  em,
+  env,
+  ex,
+  fitContent,
+  fr,
+  GeneralCSSData,
+  getMeta,
+  grad,
+  grayscale,
+  hsl,
+  hsla,
+  hueRotate,
+  hwb,
+  inset,
+  invert,
+  linearGradient,
+  matrix,
+  matrix3d,
+  max,
+  min,
+  minmax,
+  mm,
+  ms,
+  opacity,
+  path,
+  pc,
+  percent,
+  perspective,
+  polygon,
+  pt,
+  px,
+  Q,
+  quote,
+  rad,
+  radialGradient,
+  rem,
+  repeat,
+  repeatingConicGradient,
+  repeatingLinearGradient,
+  repeatingRadialGradient,
+  resetMeta,
+  rgb,
+  rgba,
+  rotate,
+  rotate3d,
+  rotateX,
+  rotateY,
+  rotateZ,
+  s,
+  saturate,
+  scale,
+  scale3d,
+  scaleX,
+  scaleY,
+  scaleZ,
+  sepia,
+  sign,
+  skew,
+  skewX,
+  skewY,
+  steps,
+  StyleHandler,
+  StyleProxy,
+  translate,
+  translate3d,
+  translateX,
+  translateY,
+  translateZ,
+  turn,
+  url,
+  useProxy,
+  vh,
+  vmax,
+  vmin,
+  vw,
+  x,
+} from "@windijs/helpers";
 import { hasKey } from "@windijs/shared";
 
-const funcs = { quote, attr, url, $var, path, hwb, dropShadow, counters, circle, ellipse, inset, polygon, matrix, matrix3d, perspective, rotate, rotate3d, rotateX, rotateY, rotateZ, scale, scale3d, scaleX, scaleY, scaleZ, skew, skewX, skewY, translate, translate3d, translateX, translateY, translateZ, steps, calc, clamp, max, min, abs, sign, blur, brightness, contrast, grayscale, invert, opacity, saturate, sepia, rgb, rgba, hsl, hsla, counter, env, minmax, repeat, hueRotate, fitContent, cubicBezier, linearGradient, radialGradient, conicGradient, repeatingConicGradient, repeatingLinearGradient, repeatingRadialGradient };
-const units = { percent, deg, grad, rad, turn, s, ms, fr, $in, dpi, dpcm, dppx, x, px, pc, pt, cm, mm, Q, ch, ex, em, rem, vw, vh, vmax, vmin };
+const funcs = {
+  quote,
+  attr,
+  url,
+  $var,
+  path,
+  hwb,
+  dropShadow,
+  counters,
+  circle,
+  ellipse,
+  inset,
+  polygon,
+  matrix,
+  matrix3d,
+  perspective,
+  rotate,
+  rotate3d,
+  rotateX,
+  rotateY,
+  rotateZ,
+  scale,
+  scale3d,
+  scaleX,
+  scaleY,
+  scaleZ,
+  skew,
+  skewX,
+  skewY,
+  translate,
+  translate3d,
+  translateX,
+  translateY,
+  translateZ,
+  steps,
+  calc,
+  clamp,
+  max,
+  min,
+  abs,
+  sign,
+  blur,
+  brightness,
+  contrast,
+  grayscale,
+  invert,
+  opacity,
+  saturate,
+  sepia,
+  rgb,
+  rgba,
+  hsl,
+  hsla,
+  counter,
+  env,
+  minmax,
+  repeat,
+  hueRotate,
+  fitContent,
+  cubicBezier,
+  linearGradient,
+  radialGradient,
+  conicGradient,
+  repeatingConicGradient,
+  repeatingLinearGradient,
+  repeatingRadialGradient,
+};
+const units = {
+  percent,
+  deg,
+  grad,
+  rad,
+  turn,
+  s,
+  ms,
+  fr,
+  $in,
+  dpi,
+  dpcm,
+  dppx,
+  x,
+  px,
+  pc,
+  pt,
+  cm,
+  mm,
+  Q,
+  ch,
+  ex,
+  em,
+  rem,
+  vw,
+  vh,
+  vmax,
+  vmin,
+};
 
-export function stylePropertyHandler<T extends Partial<Record<keyof CSSDecls, unknown | BuildFunc<string>>> = {}> (cfg: T = {} as T) {
+export function stylePropertyHandler<T extends Partial<Record<keyof CSSDecls, unknown | BuildFunc<string>>> = {}>(cfg: T = {} as T) {
   const build = (prop: string, value: string) => {
     const meta = getMeta();
     if (meta.props.length === 1) meta.props.push(value);
@@ -14,18 +209,23 @@ export function stylePropertyHandler<T extends Partial<Record<keyof CSSDecls, un
   };
   return {
     type: "style",
-    get: (prop) => useProxy(value => {
-      pushMetaProp(prop);
-      if (hasKey(cfg, prop)) {
-        const cv = cfg[prop];
-        const result = typeof cv === "function" ? cv(value) : handleConfig(v => build(prop, v as string), cv as unknown as object, "css", value);
-        if (result) return result;
-      }
-      if (value === "var") return (name: string, defaultValue?: string | undefined) => build(prop, funcs.$var(name, defaultValue));
-      if (value in funcs) return (...args: any[]) => build(prop, ((funcs as unknown as { [key: string]: Function })[value])(...args));
-      if (value === "in") return useProxy(v => build(prop, units.$in[+v].toString()));
-      if (value in units && value !== "color") return useProxy(v => build(prop, (units as any)[value][v].toString()));
-      return build(prop, value);
-    }),
+    get: prop =>
+      useProxy(value => {
+        const meta = getMeta();
+        resetMeta("style", meta.type, [prop], meta.variants);
+        if (hasKey(cfg, prop)) {
+          const cv = cfg[prop];
+          const result = typeof cv === "function" ? cv(value) : handleConfig(v => build(prop, v as string), cv as unknown as object, "css", value);
+          if (result) return result;
+        }
+        if (value === "var") return (name: string, defaultValue?: string | undefined) => build(prop, funcs.$var(name, defaultValue));
+
+        if (value in funcs) return (...args: unknown[]) => build(prop, (funcs as unknown as { [key: string]: Function })[value](...args));
+
+        if (value === "in") return useProxy(v => build(prop, units.$in[+v].toString()));
+        if (hasKey(units, value)) return useProxy(v => build(prop, (units[value] as Record<string, number>)[v].toString()));
+
+        return build(prop, value);
+      }),
   } as StyleHandler<GeneralCSSData & StyleProxy<T>>;
 }
