7141aac070ad75c49bf23f6d4a6a46b28bcc2df6
voorjaar <felitas@zoho.com>
2022-09-29 02:51:28 +0800

    test: attributify with default value

--- packages/windicss/tests/attributify.test.ts
+++ packages/windicss/tests/attributify.test.ts
@@ -21,3 +21,11 @@ const processor = new Processor({
 test("attributify with inner variants", () => {
   expect(processor.interpret('bg="red-500 hover:red-400 sm:dark:red-600"')).toMatchSnapshot();
 });
+
+test("attributify with outer variants", () => {
+  expect(processor.interpret('hover:bg="red-500 dark:red-400"')).toMatchSnapshot();
+});
+
+test("attributify with default value", () => {
+  expect(processor.interpret('grid="cols-2"')).toMatchSnapshot();
+});
