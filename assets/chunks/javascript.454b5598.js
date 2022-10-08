487f407657be324edd3593acc6c3a22ed01da4e0
voorjaar <felitas@zoho.com>
2022-10-08 23:57:52 +0800

    Updates

---
+++ assets/chunks/javascript.454b5598.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript.36889df6.js";import"./Repl.6c2a7fa0.js";import"../app.7f4135e4.js";import"./_virtual_variants.69d94320.js";import"./text.03b99c34.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.34.0(9d278685b078158491964f8fd7ac9628fffa0f30)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var n=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{n as conf,l as language};
